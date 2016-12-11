7f10ab5bd0690b7cdfee33d2c7dea0fb58195214
Rebecca Le <traybaby@gmail.com>
2016-12-11 19:57:57 +0800

    First cut at a solution for 2016 day 9 part 2 - recursive and naive.
    Works for test cases, doesn't finish with provided input.

--- lib/y2016/day09.ex
+++ lib/y2016/day09.ex
@@ -27,18 +27,58 @@ defmodule Y2016.Day09 do
     end
   end
 
+  @doc """
+  iex> Day09.advanced_decompress_length("(3x3)XYZ")
+  9 # XYZXYZYZ
+
+  iex> Day09.advanced_decompress_length("X(8x2)(3x3)ABCY")
+  20 # -> XABCABCABCABCABCABCY
+
+  iex> Day09.advanced_decompress_length("(27x12)(20x12)(13x14)(7x10)(1x12)A")
+  241920 # "A" * 241920
+
+  iex> Day09.advanced_decompress_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")
+  445
+  """
+  def advanced_decompress_length(string) do
+    case String.split(string, "(", parts: 2) do
+      [string] ->
+        String.length(string)
+
+      [content, string] ->
+        String.length(content) + length_via_marker_duplication(string)
+    end
+  end
+
+  defp length_via_marker_duplication(string) do
+    case String.split(string, ")", parts: 2) do
+      [string] ->
+        String.length(string)
+
+      [marker, string] ->
+        [dup_length, dup_count] = decode_marker(marker)
+
+        {content_to_dup, rest} = String.split_at(string, dup_length)
+
+        (String.duplicate(content_to_dup, dup_count) <> rest)
+        |> advanced_decompress_length
+    end
+  end
+
   defp duplicate_via_marker(string) do
     case String.split(string, ")", parts: 2) do
       [string] ->
         string
 
       [marker, string] ->
-        [dup_length, dup_count] = String.split(marker, "x") |> Enum.map(&String.to_integer/1)
+        [dup_length, dup_count] = decode_marker(marker)
 
         {content_to_dup, rest} = String.split_at(string, dup_length)
         String.duplicate(content_to_dup, dup_count) <> decompress(rest)
     end
   end
+
+  defp decode_marker(marker), do: String.split(marker, "x") |> Enum.map(&String.to_integer/1)
 
   def part1_verify, do: input() |> decompress() |> String.length()
 end
