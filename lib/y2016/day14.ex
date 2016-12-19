1c28e892cf98a91d831b4d2778ea745f183f57f9
Rebecca Le <traybaby@gmail.com>
2016-12-19 15:54:51 +0800

    2016 day 14 naive implementation
    Does not implement caching of hash values so a *lot* of rework is done

---
+++ lib/y2016/day14.ex
@@ -0,0 +1,99 @@
+defmodule Y2016.Day14 do
+  use Advent.Day, no: 14
+
+  @salt "zpqevtbw"
+
+  def part1 do
+    look_for_key(0, %{}, &hash/2)
+  end
+
+  def part2 do
+    look_for_key(0, %{}, &super_hash/2)
+  end
+
+  def look_for_key(index, keys, _hash_fn) when map_size(keys) == 64, do: index - 1
+
+  def look_for_key(index, keys, hash_fn) do
+    case key?(index, @salt, hash_fn) do
+      true ->
+        IO.puts("Found key #{map_size(keys) + 1}: #{index}")
+        look_for_key(index + 1, Map.put_new(keys, index, true), hash_fn)
+
+      false ->
+        look_for_key(index + 1, keys, hash_fn)
+    end
+  end
+
+  @doc """
+  iex> Day14.key?(18, "abc", &Day14.hash/2)
+  false # has triple but is not key
+
+  iex> Day14.key?(38, "abc", &Day14.hash/2)
+  false
+
+  iex> Day14.key?(39, "abc", &Day14.hash/2)
+  true
+
+  iex> Day14.key?(40, "abc", &Day14.hash/2)
+  false
+
+  iex> Day14.key?(5, "abc", &Day14.super_hash/2)
+  false # triple but not key
+
+  iex> Day14.key?(10, "abc", &Day14.super_hash/2)
+  true
+
+  iex> Day14.key?(11, "abc", &Day14.super_hash/2)
+  false
+
+  iex> Day14.key?(22551, "abc", &Day14.super_hash/2)
+  true
+  """
+  def key?(index, salt, hash_fn) do
+    {is_triple, letter} = hash_fn.(index, salt) |> is_triple?
+
+    is_triple && check_for_five_char_sequence(index, salt, letter, hash_fn)
+  end
+
+  def is_triple?(string) do
+    case Regex.run(~r/([a-z0-9])\1\1/, string) do
+      nil -> {false, nil}
+      [_triple, single] -> {true, single}
+    end
+  end
+
+  def check_for_five_char_sequence(index, salt, letter, hash_fn) do
+    Enum.any?((index + 1)..(index + 1000), fn new_index ->
+      hash_fn.(new_index, salt) |> has_five_char_sequence?(letter)
+    end)
+  end
+
+  @doc """
+  iex> Day14.has_five_char_sequence?("abc123", "a")
+  false
+
+  iex> Day14.has_five_char_sequence?("abcaaaaa123", "a")
+  true
+  """
+  def has_five_char_sequence?(string, letter) do
+    String.contains?(string, String.duplicate(letter, 5))
+  end
+
+  @doc """
+  iex> Day14.hash("abc18") |> String.contains?("cc38887a5")
+  true
+  """
+  def hash(index, salt), do: hash("#{salt}#{index}")
+  def hash(string), do: :crypto.hash(:md5, string) |> Base.encode16() |> String.downcase()
+
+  @doc """
+  iex> Day14.super_hash(0, "abc")
+  "a107ff634856bb300138cac6568c0f24"
+  """
+  def super_hash(index, salt), do: "#{salt}#{index}" |> do_super_hash(0)
+  def do_super_hash(string, 2017), do: string
+  def do_super_hash(string, index), do: string |> hash |> do_super_hash(index + 1)
+
+  def part1_verify, do: part1()
+  def part2_verify, do: part2()
+end
---
+++ test/y2016/day14_test.exs
@@ -0,0 +1,10 @@
+defmodule Y2016.Day14Test do
+  use ExUnit.Case, async: true
+  alias Y2016.Day14
+  doctest Day14
+
+  test "verification, part 1", do: assert(Day14.part1_verify() == 16106)
+
+  @tag timeout: :infinity
+  test "verification, part 2", do: assert(Day14.part2_verify() == 22423)
+end
