cc0f17f584c32cceb2a96227a6c8ad4d60ccf2c9
Rebecca Le <traybaby@gmail.com>
2017-12-13 21:00:36 +1100

    2017 day 9 part 2

--- lib/y2017/day09.ex
+++ lib/y2017/day09.ex
@@ -2,61 +2,66 @@ defmodule Y2017.Day09 do
   use Advent.Day, no: 09
 
   @doc """
-  iex> Day09.part1("{}")
-  1
+  iex> Day09.parts("{}")
+  {1, 0}
 
-  iex> Day09.part1("{{{}}}")
-  6
+  iex> Day09.parts("{{{}}}")
+  {6, 0}
 
-  iex> Day09.part1("{{},{}}")
-  5
+  iex> Day09.parts("{{},{}}")
+  {5, 0}
 
-  iex> Day09.part1("{{{},{},{{}}}}")
-  16
+  iex> Day09.parts("{{{},{},{{}}}}")
+  {16, 0}
 
-  iex> Day09.part1("{<a>,<a>,<a>,<a>}")
-  1
+  iex> Day09.parts("{<a>,<a>,<a>,<a>}")
+  {1, 4}
 
-  iex> Day09.part1("{{<ab>},{<ab>},{<ab>},{<ab>}}")
-  9
+  iex> Day09.parts("{{<ab>},{<ab>},{<ab>},{<ab>}}")
+  {9, 8}
 
-  iex> Day09.part1("{{<!!>},{<!!>},{<!!>},{<!!>}}")
-  9
+  iex> Day09.parts("{{<!!>},{<!!>},{<!!>},{<!!>}}")
+  {9, 0}
 
-  iex> Day09.part1("{{<a!>},{<a!>},{<a!>},{<ab>}}")
-  3
+  iex> Day09.parts("{{<a!>},{<a!>},{<a!>},{<ab>}}")
+  {3, 17}
   """
-  def part1(input), do: do_part1(String.codepoints(input), {:group, 0}, 0)
+  def parts(input), do: do_parts(String.codepoints(input), {:group, 0}, {0, 0})
 
   # Base case
-  def do_part1([], {:group, 0}, score), do: score
+  def do_parts([], {:group, 0}, results), do: results
 
   # Group terminators
-  def do_part1(["{" | rest], {:group, level}, score) do
-    do_part1(rest, {:group, level + 1}, score + level + 1)
+  def do_parts(["{" | rest], {:group, level}, {score, cancelled}) do
+    do_parts(rest, {:group, level + 1}, {score + level + 1, cancelled})
   end
 
-  def do_part1(["}" | rest], {:group, level}, score) do
-    do_part1(rest, {:group, level - 1}, score)
+  def do_parts(["}" | rest], {:group, level}, results) do
+    do_parts(rest, {:group, level - 1}, results)
   end
 
   # Garbage terminators
-  def do_part1(["<" | rest], {:group, level}, score) do
-    do_part1(rest, {:garbage, level}, score)
+  def do_parts(["<" | rest], {:group, level}, results) do
+    do_parts(rest, {:garbage, level}, results)
   end
 
-  def do_part1([">" | rest], {:garbage, level}, score) do
-    do_part1(rest, {:group, level}, score)
+  def do_parts([">" | rest], {:garbage, level}, results) do
+    do_parts(rest, {:group, level}, results)
   end
 
   # Everything else
-  def do_part1(["!", _ | rest], state, score) do
-    do_part1(rest, state, score)
+  def do_parts(["!", _ | rest], state, results) do
+    do_parts(rest, state, results)
   end
 
-  def do_part1([_ | rest], state, score) do
-    do_part1(rest, state, score)
+  def do_parts([_ | rest], {:group, _} = state, results) do
+    do_parts(rest, state, results)
   end
 
-  def part1_verify, do: input() |> part1()
+  def do_parts([_ | rest], {:garbage, _} = state, {score, cancelled}) do
+    do_parts(rest, state, {score, cancelled + 1})
+  end
+
+  def part1_verify, do: input() |> parts() |> elem(0)
+  def part2_verify, do: input() |> parts() |> elem(1)
 end
--- test/y2017/day09_test.exs
+++ test/y2017/day09_test.exs
@@ -4,4 +4,5 @@ defmodule Y2017.Day09Test do
   doctest Day09
 
   test "verification, part 1", do: assert(Day09.part1_verify() == 16869)
+  test "verification, part 2", do: assert(Day09.part2_verify() == 7284)
 end
