6108bc6091b69d9a9882800980002c09b4d0ca87
Rebecca Le <traybaby@gmail.com>
2017-12-17 17:34:05 +1100

    2017 day 12 part 1

---
+++ lib/y2017/day12.ex
@@ -0,0 +1,46 @@
+defmodule Y2017.Day12 do
+  use Advent.Day, no: 12
+
+  def part1(input, subject) do
+    find_all_connected(subject, input, [subject])
+    |> Enum.uniq()
+    |> Enum.sort()
+  end
+
+  defp find_all_connected(subject, data, seen) do
+    subject
+    |> find_immediately_connected(data)
+    |> Enum.reject(&Enum.member?(seen, &1))
+    |> Enum.reduce(seen, &find_all_connected(&1, data, [&1 | &2]))
+  end
+
+  defp find_immediately_connected(subject, data) do
+    Map.fetch!(data, subject) ++ find_indirectly_connected(subject, data)
+  end
+
+  defp find_indirectly_connected(subject, data) do
+    data
+    |> Enum.filter(fn {_, v} -> Enum.member?(v, subject) end)
+    |> Enum.map(&elem(&1, 1))
+    |> List.flatten()
+  end
+
+  def parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.reduce(%{}, &parse_line/2)
+  end
+
+  defp parse_line(line, acc) do
+    [key, vals] = String.split(line, " <-> ", parts: 2)
+
+    Map.put(
+      acc,
+      String.to_integer(key),
+      String.split(vals, ", ") |> Enum.map(&String.to_integer/1)
+    )
+  end
+
+  def part1_verify, do: input() |> parse_input() |> part1(0) |> length
+end
---
+++ test/y2017/day12_test.exs
@@ -0,0 +1,37 @@
+defmodule Y2017.Day12Test do
+  use ExUnit.Case, async: true
+  alias Y2017.Day12
+  doctest Day12
+
+  test "verification, part 1", do: assert(Day12.part1_verify() == 128)
+
+  test "input parsing" do
+    output = Day12.input("../../../test/y2017/input/day12") |> Day12.parse_input()
+
+    assert output == %{
+             0 => [2],
+             1 => [1],
+             2 => [0, 3, 4],
+             3 => [2, 4],
+             4 => [2, 3, 6],
+             5 => [6],
+             6 => [4, 5]
+           }
+  end
+
+  test "part 1" do
+    assert Day12.part1(
+             %{
+               0 => [2],
+               1 => [1],
+               2 => [0, 3, 4],
+               3 => [2, 4],
+               4 => [2, 3, 6],
+               5 => [6],
+               6 => [4, 5]
+             },
+             0
+           ) ==
+             [0, 2, 3, 4, 5, 6]
+  end
+end
