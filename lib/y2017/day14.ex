e3efc26a4a9e7180d6272fc9e31dcc4ddb628dab
Rebecca Le <traybaby@gmail.com>
2017-12-20 18:26:54 +0800

    2017 day 14 part 2

--- lib/y2017/day14.ex
+++ lib/y2017/day14.ex
@@ -15,41 +15,82 @@ defmodule Y2017.Day14 do
     (size - 1)..0
     |> Enum.to_list()
     |> Stream.map(&calculate_hash(input, &1))
-    |> Stream.map(&count_bits(&1, div(size, 4)))
+    |> Enum.map(&to_bits(&1, div(size, 4)))
+    |> List.flatten()
     |> Enum.sum()
   end
 
   @doc """
-  iex> Day14.count_bits("a0c2017", 7)
-  9
+  Same as above, use the 8x8 grid for test purposes. The example in the question's answer is 8, but
+  that includes off screen things. When only considering the 8x8 grid, it's 12.
+
+  iex> Day14.part2("flqrgnkx", 8)
+  12
   """
-  def count_bits(string, size) do
+  def part2(input \\ @puzzle_input, size) do
+    list = Enum.to_list(0..(size - 1))
+
+    list
+    |> Stream.map(&calculate_hash(input, &1))
+    |> Stream.map(&to_bits(&1, div(size, 4)))
+    |> Stream.zip(list)
+    |> Enum.reduce([], &convert_to_points(&1, &2, 0))
+    |> count_groups(0)
+  end
+
+  @doc """
+  iex> Day14.to_bits("a0c2017", 7)
+  [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1]
+  """
+  def to_bits(string, size) do
     string
     |> String.codepoints()
     |> Enum.take(size)
     |> Enum.map(&bits/1)
-    |> Enum.sum()
+    |> List.flatten()
   end
 
   defp bits(char) do
     case char do
-      "0" -> 0
-      "1" -> 1
-      "2" -> 1
-      "3" -> 2
-      "4" -> 1
-      "5" -> 2
-      "6" -> 2
-      "7" -> 3
-      "8" -> 1
-      "9" -> 2
-      "a" -> 2
-      "b" -> 3
-      "c" -> 2
-      "d" -> 3
-      "e" -> 3
-      "f" -> 4
+      "0" -> [0, 0, 0, 0]
+      "1" -> [0, 0, 0, 1]
+      "2" -> [0, 0, 1, 0]
+      "3" -> [0, 0, 1, 1]
+      "4" -> [0, 1, 0, 0]
+      "5" -> [0, 1, 0, 1]
+      "6" -> [0, 1, 1, 0]
+      "7" -> [0, 1, 1, 1]
+      "8" -> [1, 0, 0, 0]
+      "9" -> [1, 0, 0, 1]
+      "a" -> [1, 0, 1, 0]
+      "b" -> [1, 0, 1, 1]
+      "c" -> [1, 1, 0, 0]
+      "d" -> [1, 1, 0, 1]
+      "e" -> [1, 1, 1, 0]
+      "f" -> [1, 1, 1, 1]
     end
+  end
+
+  defp count_groups([], count), do: count
+
+  defp count_groups([elem | rest], count) do
+    {_group, not_adjacent} = split_out_group([elem], [], rest)
+    count_groups(not_adjacent, count + 1)
+  end
+
+  defp split_out_group([], seen, list), do: {seen, list}
+
+  defp split_out_group([{x, y} | coords], seen, list) do
+    possible_adjacent = [{x + 1, y}, {x, y + 1}, {x - 1, y}, {x, y - 1}]
+    {adjacent, rest} = Enum.split_with(list, &Enum.member?(possible_adjacent, &1))
+    split_out_group(adjacent ++ coords, [{x, y} | seen], rest)
+  end
+
+  defp convert_to_points({[], _}, acc, _), do: acc |> Enum.reverse()
+
+  defp convert_to_points({[key | keys], index}, acc, counter) do
+    new_acc = if key == 1, do: [{counter, index} | acc], else: acc
+    convert_to_points({keys, index}, new_acc, counter + 1)
   end
 
   defp calculate_hash(input, suffix) do
@@ -57,4 +98,5 @@ defmodule Y2017.Day14 do
   end
 
   def part1_verify, do: part1(128)
+  def part2_verify, do: part2(128)
 end
--- test/y2017/day14_test.exs
+++ test/y2017/day14_test.exs
@@ -4,4 +4,5 @@ defmodule Y2017.Day14Test do
   doctest Day14
 
   test "verification, part 1", do: assert(Day14.part1_verify() == 8216)
+  test "verification, part 2", do: assert(Day14.part2_verify() == 1139)
 end
