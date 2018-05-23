6b4950ded91d07685135ddddc203b49b7a7ff4f3
Rebecca Le <traybaby@gmail.com>
2018-05-23 15:45:51 +0800

    Refactor 2017 day 5 part 2 to use Erlang arrays instead of Elixir lists - more efficient to update values in over and over again

--- lib/y2017/day05.ex
+++ lib/y2017/day05.ex
@@ -6,7 +6,9 @@ defmodule Y2017.Day05 do
   5
   """
   def part1(input) do
-    do_jumps(input, 0, length(input), 0, &add_one/1)
+    input
+    |> :array.from_list()
+    |> do_jumps(0, length(input), 0, &add_one/1)
   end
 
   @doc """
@@ -14,7 +16,9 @@ defmodule Y2017.Day05 do
   10
   """
   def part2(input) do
-    do_jumps(input, 0, length(input), 0, &add_or_subtract/1)
+    input
+    |> :array.from_list()
+    |> do_jumps(0, length(input), 0, &add_or_subtract/1)
   end
 
   defp do_jumps(_, position, length, move_no, _) when position >= length, do: move_no
@@ -25,24 +29,24 @@ defmodule Y2017.Day05 do
   end
 
   @doc """
-  iex> Day05.next_jump([0, 3, 0, 1, -3], 0, &Day05.add_one/1)
-  {[1, 3, 0, 1, -3], 0}
+  iex> Day05.next_jump(:array.from_list([0, 3, 0, 1, -3]), 0, &Day05.add_one/1)
+  {:array.from_list([1, 3, 0, 1, -3]), 0}
 
-  iex> Day05.next_jump([1, 3, 0, 1, -3], 0, &Day05.add_one/1)
-  {[2, 3, 0, 1, -3], 1}
+  iex> Day05.next_jump(:array.from_list([1, 3, 0, 1, -3]), 0, &Day05.add_one/1)
+  {:array.from_list([2, 3, 0, 1, -3]), 1}
 
-  iex> Day05.next_jump([2, 3, 0, 1, -3], 1, &Day05.add_one/1)
-  {[2, 4, 0, 1, -3], 4}
+  iex> Day05.next_jump(:array.from_list([2, 3, 0, 1, -3]), 1, &Day05.add_one/1)
+  {:array.from_list([2, 4, 0, 1, -3]), 4}
 
-  iex> Day05.next_jump([2, 4, 0, 1, -3], 4, &Day05.add_one/1)
-  {[2, 4, 0, 1, -2], 1}
+  iex> Day05.next_jump(:array.from_list([2, 4, 0, 1, -3]), 4, &Day05.add_one/1)
+  {:array.from_list([2, 4, 0, 1, -2]), 1}
 
-  iex> Day05.next_jump([2, 4, 0, 1, -2], 1, &Day05.add_one/1)
-  {[2, 5, 0, 1, -2], 5}
+  iex> Day05.next_jump(:array.from_list([2, 4, 0, 1, -2]), 1, &Day05.add_one/1)
+  {:array.from_list([2, 5, 0, 1, -2]), 5}
   """
   def next_jump(input, position, fun) do
-    {move, input} = List.pop_at(input, position)
-    new_input = List.insert_at(input, position, fun.(move))
+    move = :array.get(position, input)
+    new_input = :array.set(position, fun.(move), input)
 
     {new_input, position + move}
   end
