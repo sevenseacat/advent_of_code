00e3213be0540ff4b8f37ab0abb770521f00c8e3
Rebecca Le <traybaby@gmail.com>
2018-12-06 16:34:42 +0800

    2018 day 6 - stop carrying the label of the point around, only use the label when displaying the debug grid

--- lib/y2018/day06.ex
+++ lib/y2018/day06.ex
@@ -1,34 +1,32 @@
 defmodule Y2018.Day06 do
   use Advent.Day, no: 6
 
-  @ids ~w/a b c d e f g h i j k l m n o p q r s t u v w x y z ! @ # $ % ^ & * ( ) - _ + = : ; < > , ? A B C S T F R N M P |/
+  @markers ~w/a b c d e f g h i j k l m n o p q r s t u v w x y z ! @ # $ % ^ & * ( ) - _ + = : ; < > , ? A B C S T F R N M P |/
 
   @doc """
   iex> Day06.part1("1, 1\\n1, 6\\n8, 3\\n3, 4\\n5, 5\\n8, 9")
   {{5, 5}, 17}
   """
   def part1(input) do
-    points =
-      parse_input(input)
-      |> Enum.zip(@ids)
+    points = parse_input(input)
 
-    {{min_x, min_y}, {max_x, max_y}} = bounds_of_grid(points)
-    manhattan_grid = manhattan_grid(points, {min_x, min_y}, {max_x, max_y})
+    {mins, maxes} = bounds_of_grid(points)
+    manhattan_grid = manhattan_grid(points, mins, maxes)
 
-    {{x, y}, count} =
+    {point, count} =
       points
-      |> Enum.reject(fn coord ->
-        infinite_area?(manhattan_grid, coord, {min_x, min_y}, {max_x, max_y})
+      |> Enum.reject(fn point ->
+        infinite_area?(manhattan_grid, point, mins, maxes)
       end)
-      |> Enum.map(fn {coord, letter} ->
-        {coord, Enum.count(manhattan_grid, fn {_, char_letter} -> char_letter == letter end)}
+      |> Enum.map(fn point ->
+        {point, Enum.count(manhattan_grid, fn {_, closest} -> closest == point end)}
       end)
       |> Enum.sort_by(fn {_, count} -> -count end)
       |> hd
 
-    # display_grid(manhattan_grid, {min_x, min_y}, {max_x, max_y}, {x, y})
+    # display_grid(manhattan_grid, points, mins, maxes, point)
 
-    {{x, y}, count}
+    {point, count}
   end
 
   @doc """
@@ -36,9 +34,7 @@ defmodule Y2018.Day06 do
   16
   """
   def part2(input, score_limit) do
-    points =
-      parse_input(input)
-      |> Enum.zip(@ids)
+    points = parse_input(input)
 
     {mins, maxes} = bounds_of_grid(points)
 
@@ -47,20 +43,20 @@ defmodule Y2018.Day06 do
   end
 
   defp bounds_of_grid(points) do
-    {{{min_x, _}, _}, {{max_x, _}, _}} = Enum.min_max_by(points, fn {{x, _}, _} -> x end)
-    {{{_, min_y}, _}, {{_, max_y}, _}} = Enum.min_max_by(points, fn {{_, y}, _} -> y end)
+    {{min_x, _}, {max_x, _}} = Enum.min_max_by(points, fn {x, _} -> x end)
+    {{_, min_y}, {_, max_y}} = Enum.min_max_by(points, fn {_, y} -> y end)
 
     {{min_x, min_y}, {max_x, max_y}}
   end
 
   @doc """
-  iex> points = [{{1, 1}, nil}, {{1, 6}, nil}, {{8, 3}, nil}, {{3, 4}, nil}, {{5, 5}, nil}, {{8, 9}, nil}]
+  iex> points = [{1, 1}, {1, 6}, {8, 3}, {3, 4}, {5, 5}, {8, 9}]
   iex> Day06.safety_score({4, 3}, points)
   30
   """
   def safety_score(coord, points) do
     points
-    |> Enum.map(fn {point, _} -> manhattan_distance(point, coord) end)
+    |> Enum.map(fn point -> manhattan_distance(point, coord) end)
     |> Enum.sum()
   end
 
@@ -71,31 +67,31 @@ defmodule Y2018.Day06 do
         coord
         |> all_manhattan_distances(points)
         |> Enum.sort_by(fn {_, distance} -> distance end)
-        |> get_winning_letter
+        |> get_owning_coord
 
       Map.put(acc, coord, owner)
     end)
   end
 
-  defp infinite_area?(grid, {_, letter}, {min_x, min_y}, {max_x, max_y}) do
-    Enum.any?(grid, fn {{x, y}, grid_letter} ->
-      letter == grid_letter && (x == min_x || x == max_x || y == min_y || y == max_y)
+  defp infinite_area?(grid, point, {min_x, min_y}, {max_x, max_y}) do
+    Enum.any?(grid, fn {{x, y}, owner} ->
+      owner == point && (x == min_x || x == max_x || y == min_y || y == max_y)
     end)
   end
 
   defp all_manhattan_distances(coord, points) do
     points
-    |> Enum.reduce([], fn {point, letter}, acc ->
-      [{letter, manhattan_distance(coord, point)} | acc]
+    |> Enum.reduce([], fn point, acc ->
+      [{point, manhattan_distance(coord, point)} | acc]
     end)
   end
 
   defp manhattan_distance({x1, y1}, {x2, y2}), do: abs(x1 - x2) + abs(y1 - y2)
 
-  defp get_winning_letter([{letter, x}, {_, y} | _]) do
+  defp get_owning_coord([{coord, x}, {_, y} | _]) do
     case x == y do
-      true -> "."
-      false -> letter
+      true -> nil
+      false -> coord
     end
   end
 
@@ -122,11 +118,17 @@ defmodule Y2018.Day06 do
   Debugging function to print out the grid with different characters for different territory.
   Allows a square to be highlighted - finally showing me that my infinite area test was borked.
   """
-  def display_grid(map, {x1, y1}, {x2, y2}, {highlight_x, highlight_y}) do
+  def display_grid(map, points, {x1, y1}, {x2, y2}, {highlight_x, highlight_y} \\ {0, 0}) do
     IO.puts("")
 
+    markers = Enum.zip(points, @markers) |> Enum.into(%{})
+
     for y <- y1..y2, x <- x1..x2 do
-      if x == highlight_x and y == highlight_y, do: "█", else: Map.get(map, {x, y})
+      if x == highlight_x and y == highlight_y do
+        "█"
+      else
+        Map.get(markers, Map.get(map, {x, y}), ".")
+      end
     end
     |> Enum.chunk_every(x2 - x1 + 1)
     |> Enum.map(&Enum.join/1)
