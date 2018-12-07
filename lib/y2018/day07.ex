c57702665c9a5bb293c38a2df5a6f8414009830b
Rebecca Le <traybaby@gmail.com>
2018-12-07 17:23:39 +0800

    2018 day 7, part 1 - yep, I was totally overthinking it

--- lib/y2018/day07.ex
+++ lib/y2018/day07.ex
@@ -1,16 +1,61 @@
 defmodule Y2018.Day07 do
+  use Advent.Day, no: 7
+
   @doc """
   iex> Day07.part1([{"A", "C"}, {"F", "C"}, {"B", "A"}, {"D", "A"}, {"E", "B"}, {"E", "D"}, {"E", "F"}])
   "CABDFE"
   """
   def part1(input) do
+    input = to_dependency_list(input)
+
+    start = find_next(input) |> hd
+
+    do_part1(input, start, [])
+    |> Enum.reverse()
+    |> List.to_string()
+  end
+
+  defp do_part1(input, letter, seen) do
+    # This letter is seen - mark it as so and remove it from all of the dependency lists.
+    seen = [letter | seen]
+    input = clear_dependencies(input, letter)
+    next = find_next(input)
+
+    case next do
+      [] -> seen
+      x -> do_part1(input, hd(x), seen)
+    end
+  end
+
+  defp clear_dependencies(input, letter) do
+    input
+    |> Enum.map(fn {x, list} -> {x, Enum.reject(list, &(&1 == letter))} end)
+    |> Enum.reject(fn {x, _} -> x == letter end)
+    |> Enum.into(%{})
+  end
+
+  @doc """
+  iex> Day07.to_dependency_list([{"A", "C"}, {"F", "C"}, {"B", "A"}, {"D", "A"}, {"E", "B"}, {"E", "D"}, {"E", "F"}])
+  %{"A" => ["C"], "B" => ["A"], "C" => [], "D" => ["A"], "E" => ["F", "D", "B"], "F" => ["C"]}
+  """
+  def to_dependency_list(input) do
+    map = letters(input) |> Enum.reduce(%{}, fn x, acc -> Map.put(acc, x, []) end)
+
+    input
+    |> Enum.reduce(map, fn {x, y}, acc -> Map.update(acc, x, [y], &[y | &1]) end)
+  end
+
+  defp find_next(input) do
+    input
+    |> Stream.filter(fn {_, list} -> list == [] end)
+    |> Stream.map(&elem(&1, 0))
+    |> Enum.sort()
   end
 
   def letters(input) do
     input
     |> Enum.reduce([], fn {x, y}, list -> [x, y | list] end)
     |> Enum.uniq()
-    |> Enum.sort()
   end
 
   @doc """
@@ -29,4 +74,6 @@ defmodule Y2018.Day07 do
        ) do
     {y, x}
   end
+
+  def part1_verify, do: input() |> parse_input() |> part1()
 end
--- test/y2018/day07_test.exs
+++ test/y2018/day07_test.exs
@@ -2,4 +2,6 @@ defmodule Y2018.Day07Test do
   use ExUnit.Case, async: true
   alias Y2018.Day07
   doctest Day07
+
+  test "verification, part 1", do: assert(Day07.part1_verify() == "JMQZELVYXTIGPHFNSOADKWBRUC")
 end
