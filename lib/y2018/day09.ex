3d6c6a5a2ac973cea5b18abcfcdc224b56614dcd
Rebecca Le <traybaby@gmail.com>
2018-12-10 00:33:06 +0800

    2018 day 9, part 2
    I'll clean up the code in the morning, but for now the solution is correct :D

--- lib/y2018/day09.ex
+++ lib/y2018/day09.ex
@@ -23,14 +23,33 @@ defmodule Y2018.Day09 do
   def part1(input) do
     {players, points} = parse_input(input)
 
+    field = :digraph.new()
+    :digraph.add_vertex(field, 0)
+    :digraph.add_vertex(field, 1)
+    :digraph.add_vertex(field, 2)
+    :digraph.add_vertex(field, 3)
+    :digraph.add_vertex(field, 4)
+    :digraph.add_vertex(field, 5)
+    :digraph.add_vertex(field, 6)
+    :digraph.add_vertex(field, 7)
+
+    :digraph.add_edge(field, 0, 4)
+    :digraph.add_edge(field, 4, 2)
+    :digraph.add_edge(field, 2, 5)
+    :digraph.add_edge(field, 5, 1)
+    :digraph.add_edge(field, 1, 6)
+    :digraph.add_edge(field, 6, 3)
+    :digraph.add_edge(field, 3, 7)
+    :digraph.add_edge(field, 7, 0)
+
     run_game(%{
       scores: %{},
       players: players,
-      turn: 0,
-      current: 1,
-      last: 0,
+      turn: 7,
+      current: 8,
+      last: 7,
       max: points,
-      field: [0]
+      field: field
     })
     |> Map.get(:scores)
     |> Enum.max_by(fn {_player, score} -> score end)
@@ -66,44 +85,36 @@ defmodule Y2018.Day09 do
     })
   end
 
-  @doc """
-  iex> Day09.place_marble([0], 1, 0)
-  {0, 1, [0, 1]}
+  defp place_marble(graph, new, last) do
+    if rem(new, 23) == 0 do
+      first = :digraph.in_neighbours(graph, last) |> hd
+      second = :digraph.in_neighbours(graph, first) |> hd
+      third = :digraph.in_neighbours(graph, second) |> hd
+      fourth = :digraph.in_neighbours(graph, third) |> hd
+      fifth = :digraph.in_neighbours(graph, fourth) |> hd
+      sixth = :digraph.in_neighbours(graph, fifth) |> hd
+      seventh = :digraph.in_neighbours(graph, sixth) |> hd
+      eighth = :digraph.in_neighbours(graph, seventh) |> hd
 
-  iex> Day09.place_marble([0, 1], 2, 1)
-  {0, 1, [0, 2, 1]}
+      :digraph.del_path(graph, eighth, seventh)
+      :digraph.del_path(graph, seventh, sixth)
+      :digraph.del_vertex(graph, seventh)
+      :digraph.add_edge(graph, eighth, sixth)
 
-  iex> Day09.place_marble([0, 2, 1], 3, 1)
-  {0, 3, [0, 2, 1, 3]}
-
-  iex> Day09.place_marble([0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15],
-  ...> 23, 13)
-  {32, 6, [0, 16, 8, 17, 4, 18, 19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15]}
-  """
-  def place_marble(field, points, last) do
-    turn =
-      if rem(points, 23) == 0 do
-        points - 2 * (div(points, 23) - 1)
-      else
-        points - 2 * div(points, 23)
-      end
-
-    if rem(points, 23) == 0 do
-      new_index = new_special_position(turn, last)
-      {replaced, list} = List.pop_at(field, new_index)
-      {points + replaced, new_index, list}
+      {new + seventh, sixth, graph}
     else
-      new_index = new_normal_position(turn, last)
-      {0, new_index, List.insert_at(field, new_index, points)}
+      :digraph.add_vertex(graph, new)
+      first = :digraph.out_neighbours(graph, last) |> hd
+      second = :digraph.out_neighbours(graph, first) |> hd
+      :digraph.del_path(graph, first, second)
+      :digraph.add_edge(graph, first, new)
+      :digraph.add_edge(graph, new, second)
+      {0, new, graph}
     end
   end
 
   def new_special_position(size, pos) do
     if pos - 7 >= 0, do: pos - 7, else: pos - 7 + size
-  end
-
-  def new_normal_position(size, pos) do
-    if pos + 2 > size, do: pos + 2 - size, else: pos + 2
   end
 
   defp parse_input(input) do
@@ -114,4 +125,5 @@ defmodule Y2018.Day09 do
   end
 
   def part1_verify, do: part1("429 players; last marble is worth 70901 points")
+  def part2_verify, do: part1("429 players; last marble is worth 7090100 points")
 end
--- test/y2018/day09_test.exs
+++ test/y2018/day09_test.exs
@@ -4,4 +4,5 @@ defmodule Y2018.Day09Test do
   doctest Day09
 
   test "verification, part 1", do: assert(Day09.part1_verify() == 399_645)
+  test "verification, part 2", do: assert(Day09.part2_verify() == 3_352_507_536)
 end
