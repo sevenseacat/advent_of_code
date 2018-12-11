bc5b2e16eae8bb795a10a2907013adca9e3f77a7
Rebecca Le <traybaby@gmail.com>
2018-12-12 00:28:21 +0800

    2018 day 11 - reimplement using a summed-area table instead of my naive approach. Math!

--- lib/y2018/day11.ex
+++ lib/y2018/day11.ex
@@ -1,3 +1,7 @@
+# This wouldn't have been possible without Reddit and the hint that I wanted to use a summed-area table.
+# https://en.wikipedia.org/wiki/Summed-area_table
+# Well, it would have been, but my naive implementation literally took hours to come up with an answer.
+# This takes a few seconds.
 defmodule Y2018.Day11 do
   use Advent.Day, no: 11
 
@@ -6,19 +10,15 @@ defmodule Y2018.Day11 do
 
   @doc """
   iex> Day11.part1(18)
-  {{33, 45}, 29}
+  {{33, 45, 3}, 29}
 
   iex> Day11.part1(42)
-  {{21, 61}, 30}
+  {{21, 61, 3}, 30}
   """
   def part1(serial \\ @serial) do
-    build_grid({0, 0}, {@max_size, @max_size}, serial)
-    |> max_by({3, 3})
-  end
-
-  def part1_ets(serial \\ @serial) do
-    build_grid_ets({0, 0}, {@max_size, @max_size}, serial)
-    |> all_maxes(3, 3)
+    serial
+    |> build_grid
+    |> max_by_summed_area_table(3, 3)
   end
 
   @doc """
@@ -29,8 +29,9 @@ defmodule Y2018.Day11 do
   {{232,251,12}, 119}
   """
   def part2(serial \\ @serial) do
-    build_grid_ets({0, 0}, {@max_size, @max_size}, serial)
-    |> all_maxes(1, 300)
+    serial
+    |> build_grid
+    |> max_by_summed_area_table(1, @max_size)
   end
 
   @doc """
@@ -56,85 +57,45 @@ defmodule Y2018.Day11 do
     |> Kernel.-(5)
   end
 
-  @doc """
-  iex> Day11.build_grid({32, 44}, {33, 45}, 18)
-  %{{32, 44} => -2, {33, 44} => -4, {32, 45} => -4, {33, 45} => 4}
-  """
-  def build_grid({min_x, min_y}, {max_x, max_y}, serial) do
-    for x <- min_x..max_x, y <- min_y..max_y do
+  def build_grid(serial) do
+    for x <- 1..@max_size, y <- 1..@max_size do
       {{x, y}, cell_power({x, y}, serial)}
     end
     |> Enum.into(%{})
   end
 
-  def build_grid_ets({min_x, min_y}, {max_x, max_y}, serial) do
-    grid = :ets.new(:day112, [:set])
+  defp max_by_summed_area_table(grid, min_size, max_size) do
+    sat = summed_area_table(grid)
 
-    for x <- min_x..max_x, y <- min_y..max_y do
-      :ets.insert(grid, {{x, y}, cell_power({x, y}, serial)})
-    end
-
-    grid
-  end
-
-  defp all_maxes(grid, min_width, max_width) do
-    min_width..max_width
-    |> Task.async_stream(
-      fn width ->
-        Enum.reduce(1..(@max_size - width), {nil, 0}, fn x, {coord, power} ->
-          {new_coord, new_power} =
-            Enum.reduce(1..(@max_size - width), {nil, 0}, fn y, {coord, power} ->
-              new_power = subgrid_power_ets(grid, {x, y}, width)
-
-              if new_power > power do
-                # IO.puts("new max is #{new_power} from #{x},#{y}")
-                {{x, y, width}, new_power}
-              else
-                {coord, power}
-              end
-            end)
-
-          if new_power > power do
-            # IO.puts("new max is #{new_power} from #{elem(new_coord, 0)},#{elem(new_coord, 1)}")
-            {new_coord, new_power}
-          else
-            {coord, power}
-          end
-        end)
-      end,
-      timeout: 600_000,
-      max_concurrency: 10
-    )
-    |> Enum.max_by(fn {:ok, {_, power}} -> power end)
-    |> elem(1)
-  end
-
-  defp max_by(grid, {width, height}) do
-    Enum.map(grid, fn {{x, y}, _} ->
-      if x + width > @max_size || y + height > @max_size do
-        # skip it.
-        {{x, y}, 0}
-      else
-        {{x, y}, subgrid_power(grid, {x, y}, width, height)}
-      end
-    end)
+    min_size..max_size
+    |> Enum.map(fn size -> max_grid_by_size(sat, size) end)
     |> Enum.max_by(fn {_, power} -> power end)
   end
 
-  defp subgrid_power_ets(grid, {corner_x, corner_y}, size) do
-    for x <- corner_x..(corner_x + size - 1),
-        y <- corner_y..(corner_y + size - 1) do
-      :ets.lookup(grid, {x, y})
-    end
-    |> Enum.reduce(0, fn [{_val, power}], acc -> acc + power end)
+  defp summed_area_table(grid) do
+    Enum.reduce(@max_size..1, %{}, fn x, x_acc ->
+      Enum.reduce(@max_size..1, x_acc, fn y, sat ->
+        Map.put(
+          sat,
+          {x, y},
+          Map.get(grid, {x, y}, 0) + Map.get(sat, {x + 1, y}, 0) + Map.get(sat, {x, y + 1}, 0) -
+            Map.get(sat, {x + 1, y + 1}, 0)
+        )
+      end)
+    end)
   end
 
-  defp subgrid_power(grid, {corner_x, corner_y}, width, height) do
-    for x <- corner_x..(corner_x + width - 1),
-        y <- corner_y..(corner_y + height - 1) do
-      Map.get(grid, {x, y})
-    end
-    |> Enum.sum()
+  defp max_grid_by_size(sat, size) do
+    sat
+    # Only want full subgrids inside the 300x300 grid
+    |> Stream.filter(fn {{x, y}, _} -> x + size - 1 <= @max_size && y + size - 1 <= @max_size end)
+    |> Enum.map(fn {{x, y}, power} ->
+      # The magic of the summed-area table at work. A + D - B - C.
+      {{x, y, size},
+       power + Map.get(sat, {x + size, y + size}, 0) - Map.get(sat, {x, y + size}, 0) -
+         Map.get(sat, {x + size, y}, 0)}
+    end)
+    |> Enum.max_by(fn {_, power} -> power end)
   end
 
   def part1_verify, do: part1(@serial) |> elem(0)
--- test/y2018/day11_test.exs
+++ test/y2018/day11_test.exs
@@ -3,5 +3,6 @@ defmodule Y2018.Day11Test do
   alias Y2018.Day11
   doctest Day11
 
-  test "verification, part 1", do: assert(Day11.part1_verify() == {34, 72})
+  test "verification, part 1", do: assert(Day11.part1_verify() == {34, 72, 3})
+  test "verification, part 2", do: assert(Day11.part2_verify() == {233, 187, 13})
 end
