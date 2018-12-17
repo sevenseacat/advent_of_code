7e98b966700fdbaa7f1cc7ca85711b23eaa34703
Rebecca Le <traybaby@gmail.com>
2018-12-17 14:32:30 +0800

    2018 day 17 - initial part 1 implementation but not complete
    How can water go left *and* right?

--- lib/y2018/day17.ex
+++ lib/y2018/day17.ex
@@ -2,31 +2,81 @@ defmodule Y2018.Day17 do
   use Advent.Day, no: 17
 
   @doc """
-  iex> Day17.part1("x=495, y=2..7\\ny=7, x=495..501\\nx=501, y=3..7\\nx=498, y=2..4\\nx=506, y=1..2\\nx=498, y=10..13\\nx=504, y=10..13\\ny=13, x=498..504")
+  iex> File.read!("test/y2018/input/day17/part1.txt") |> Day17.part1()
   57
   """
   def part1(input) do
+    input = parse_input(input)
+    {_max_x, max_y} = input |> Map.get(:clay) |> Enum.max_by(fn {_, y} -> y end)
+
+    field = run_water(input, {500, 0}, max_y)
+
+    MapSet.union(field.water, field.touched)
+    |> MapSet.size()
+  end
+
+  defp run_water(field, from, max_y) do
+    {field, settled} = drip(field, from, max_y)
+
+    if settled do
+      run_water(field, from, max_y)
+    else
+      field
+    end
   end
 
   @doc """
-  iex> Day17.parse_input("x=495, y=2..7\\ny=7, x=495..501\\nx=501, y=3..7\\nx=498, y=2..4\\nx=506, y=1..2\\nx=498, y=10..13\\nx=504, y=10..13\\ny=13, x=498..504")
+  iex> File.read!("test/y2018/input/day17/part1.txt") |> Day17.parse_input() |> Day17.drip({500, 0}) |> Map.delete(:clay)
   %{
-    {506, 1} => :clay, {495, 2} => :clay, {498, 2} => :clay, {506, 2} => :clay,
-    {495, 3} => :clay, {498, 3} => :clay, {501, 3} => :clay, {495, 4} => :clay,
-    {498, 4} => :clay, {501, 4} => :clay, {495, 5} => :clay, {501, 5} => :clay,
-    {495, 6} => :clay, {501, 6} => :clay, {495, 7} => :clay, {496, 7} => :clay,
-    {497, 7} => :clay, {498, 7} => :clay, {499, 7} => :clay, {500, 7} => :clay,
-    {501, 7} => :clay, {498, 10} => :clay, {504, 10} => :clay, {498, 11} => :clay,
-    {504, 11} => :clay, {498, 12} => :clay, {504, 12} => :clay, {498, 13} => :clay,
-    {499, 13} => :clay, {500, 13} => :clay, {501, 13} => :clay, {502, 13} => :clay,
-    {503, 13} => :clay, {504, 13} => :clay
+    touched: MapSet.new([{500, 0}, {500, 1}, {500, 2}, {500, 3}, {500, 4}, {500, 5}, {500, 6},
+    {499, 6}, {498, 6}, {497, 6}]),
+    water: MapSet.new([{496, 6}])
+  }
+  """
+  def drip(field, {_, max_y}, max_y), do: {field, false}
+
+  def drip(field, {x, y}, max_y, direction \\ :down) do
+    field = Map.update!(field, :touched, fn touched -> MapSet.put(touched, {x, y}) end)
+
+    if permeable?(field, {x, y + 1}) do
+      drip(field, {x, y + 1}, max_y)
+    else
+      if Enum.member?([:down, :left], direction) && permeable?(field, {x - 1, y}) do
+        drip(field, {x - 1, y}, max_y, :left)
+      else
+        if permeable?(field, {x + 1, y}) do
+          drip(field, {x + 1, y}, max_y, :right)
+        else
+          {Map.update!(field, :water, fn water -> MapSet.put(water, {x, y}) end), true}
+        end
+      end
+    end
+  end
+
+  defp permeable?(field, coord) do
+    !MapSet.member?(field.clay, coord) && !MapSet.member?(field.water, coord)
+  end
+
+  @doc """
+  iex> File.read!("test/y2018/input/day17/part1.txt") |> Day17.parse_input()
+  %{
+    clay: MapSet.new([{506, 1}, {495, 2}, {498, 2}, {506, 2}, {495, 3}, {498, 3},
+    {501, 3}, {495, 4}, {498, 4}, {501, 4}, {495, 5}, {501, 5}, {495, 6},
+    {501, 6}, {495, 7}, {496, 7}, {497, 7}, {498, 7}, {499, 7}, {500, 7},
+    {501, 7}, {498, 10}, {504, 10}, {498, 11}, {504, 11}, {498, 12}, {504, 12},
+    {498, 13}, {499, 13}, {500, 13}, {501, 13}, {502, 13}, {503, 13}, {504, 13}]),
+    touched: MapSet.new(),
+    water: MapSet.new()
   }
   """
   def parse_input(input) do
-    input
-    |> String.split("\n", trim: true)
-    |> Enum.map(&parse_row/1)
-    |> Enum.reduce(%{}, &to_clay_field/2)
+    clay =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse_row/1)
+      |> Enum.reduce(MapSet.new(), &to_clay_field/2)
+
+    %{clay: clay, touched: MapSet.new(), water: MapSet.new()}
   end
 
   defp parse_row(row) do
@@ -48,10 +98,8 @@ defmodule Y2018.Day17 do
   end
 
   defp to_clay_field(%{x: x_range, y: y_range}, field) do
-    for x <- x_range, y <- y_range do
-      {{x, y}, :clay}
-    end
-    |> Enum.into(%{})
-    |> Map.merge(field)
+    for(x <- x_range, y <- y_range, do: {x, y})
+    |> MapSet.new()
+    |> MapSet.union(field)
   end
 end
