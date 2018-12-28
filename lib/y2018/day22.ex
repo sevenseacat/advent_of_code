b2dc117c3d74d291b2b6c2f9a2107db68d5e4af0
Rebecca Le <traybaby@gmail.com>
2018-12-28 18:55:17 +0800

    2018 day 22, part 1 - naive implementation was wrong lol

--- lib/y2018/day22.ex
+++ lib/y2018/day22.ex
@@ -4,31 +4,48 @@ defmodule Y2018.Day22 do
   @depth 3558
   @target {15, 740}
 
+  @magic_x 16807
+  @magic_y 48271
+  @magic_erosion 20183
+
   @doc """
   iex> Day22.part1({10, 10}, 510)
   114
   """
   def part1(target \\ @target, depth \\ @depth) do
-    build_geological_level_table(target)
-    |> convert_to_erosion_levels(depth)
+    build_erosion_level_table(target, depth)
     |> Enum.reduce(0, fn {_coord, level}, sum ->
       sum + rem(level, 3)
     end)
   end
 
-  def build_geological_level_table({max_x, max_y}) do
+  def build_erosion_level_table({max_x, max_y}, depth) do
     # This is just a summed-area table with a few extra conditions.
     Enum.reduce(0..max_x, %{}, fn x, x_acc ->
       Enum.reduce(0..max_y, x_acc, fn y, acc ->
-        Map.put(acc, {x, y}, geo_level({x, y}, {max_x, max_y}, acc))
+        level =
+          geo_level({x, y}, {max_x, max_y}, acc)
+          |> to_erosion_level(depth)
+
+        Map.put(acc, {x, y}, level)
       end)
     end)
   end
 
-  def convert_to_erosion_levels(map, depth) do
-    Enum.reduce(map, map, fn {coord, level}, map ->
-      Map.put(map, coord, rem(level + depth, 20183))
-    end)
+  def to_erosion_level(val, depth), do: rem(val + depth, @magic_erosion)
+
+  defp geo_level({0, 0}, _target, _map), do: 0
+  defp geo_level({x, y}, {x, y}, __map), do: 0
+  defp geo_level({x, 0}, _target, _map), do: x * @magic_x
+  defp geo_level({0, y}, _target, _map), do: y * @magic_y
+
+  defp geo_level({x, y}, _target, map) do
+    # Numbers are going to get way too big if we use them natively
+    # Because we literally only care about the remainders of dividing a bunch of numbers,
+    # we can divide by the lowest common multiple of all of the things - the remainders
+    # will still be the same
+    lcm = @magic_x * @magic_y * @magic_erosion
+    rem(Map.get(map, {x - 1, y}) * Map.get(map, {x, y - 1}), lcm)
   end
 
   def display_grid(map, {max_x, max_y}) do
@@ -44,16 +61,9 @@ defmodule Y2018.Day22 do
     map
   end
 
-  defp geo_level({0, 0}, _target, _map), do: 0
-  defp geo_level({x, 0}, _target, _map), do: x * 16807
-  defp geo_level({0, y}, _target, _map), do: y * 48271
-  defp geo_level({x, y}, {x, y}, _map), do: 0
-
-  defp geo_level({x, y}, _target, map) do
-    Map.get(map, {x - 1, y}) * Map.get(map, {x, y - 1})
-  end
-
   defp get_symbol(0), do: "."
   defp get_symbol(1), do: "="
   defp get_symbol(2), do: "|"
+
+  def part1_verify, do: part1()
 end
--- test/y2018/day22_test.exs
+++ test/y2018/day22_test.exs
@@ -2,4 +2,6 @@ defmodule Y2018.Day22Test do
   use ExUnit.Case, async: true
   alias Y2018.Day22
   doctest Day22
+
+  test "verification, part 1", do: assert(Day22.part1_verify() == 11810)
 end
