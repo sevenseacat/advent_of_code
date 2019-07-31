cba16983d8d8e1993a6236de2b5f11a12ec2e17a
Rebecca Le <traybaby@gmail.com>
2019-07-31 15:46:07 +0800

    2018 day 23 part 2 - break down the big coordinate space into smaller coordinate spaces then everything magically works :D

--- lib/y2018/day23.ex
+++ lib/y2018/day23.ex
@@ -21,31 +21,62 @@ defmodule Y2018.Day23 do
   iex> Day23.part2([%{x: 10, y: 12, z: 12, r: 2}, %{x: 12, y: 14, z: 12, r: 2},
   ...>   %{x: 16, y: 12, z: 12, r: 4}, %{x: 14, y: 14, z: 14, r: 6},
   ...>   %{x: 50, y: 50, z: 50, r: 200}, %{x: 10, y: 10, z: 10, r: 5}])
-  {{12, 12, 12}, 36, 5}
+  36
   """
   def part2(bots) do
     {%{x: min_x}, %{x: max_x}} = Enum.min_max_by(bots, fn %{x: x} -> x end)
     {%{y: min_y}, %{y: max_y}} = Enum.min_max_by(bots, fn %{y: y} -> y end)
     {%{z: min_z}, %{z: max_z}} = Enum.min_max_by(bots, fn %{z: z} -> z end)
 
-    Enum.reduce(min_x..max_x, {nil, 0, 0}, fn x, x_acc ->
-      Enum.reduce(min_y..max_y, x_acc, fn y, y_acc ->
-        Enum.reduce(min_z..max_z, y_acc, fn z, {_coord, distance, bots_in_range} = acc ->
-          new_bots = count_bots_in_range({x, y, z}, bots)
-
-          if new_bots > bots_in_range ||
-               (new_bots == bots_in_range && distance_from_zero({x, y, z}) < distance) do
-            {{x, y, z}, distance_from_zero({x, y, z}), new_bots}
-          else
-            acc
-          end
-        end)
-      end)
-    end)
+    {x, y, z} = do_part2(bots, {min_x, max_x}, {min_y, max_y}, {min_z, max_z}, 10_000_000)
+    x + y + z
   end
 
-  defp count_bots_in_range({x, y, z}, bots) do
-    Enum.count(bots, fn bot -> within_radius?(bot, %{x: x, y: y, z: z}) end)
+  # Instead of iterating over all of the individual points from eg. -1000000000
+  # to 100000000, break it down into a bigger box by dividing all of the values
+  # by a given factor
+  # Working out which point is correct from -100 to 100 is easy, and then you can
+  # decrease the factor to find out which point inside that sub-box is correct
+  # Keep decreasing the factor until it's zero, in which case you have your final
+  # answer!
+  defp do_part2(_, {x, _}, {y, _}, {z, _}, 0), do: {x, y, z}
+
+  defp do_part2(bots, {min_x, max_x}, {min_y, max_y}, {min_z, max_z}, factor) do
+    {{x, y, z}, _, _} =
+      Enum.reduce(div(min_x, factor)..div(max_x, factor), {nil, 0, 0}, fn x, x_acc ->
+        Enum.reduce(div(min_y, factor)..div(max_y, factor), x_acc, fn y, y_acc ->
+          Enum.reduce(div(min_z, factor)..div(max_z, factor), y_acc, fn z,
+                                                                        {_coord, distance,
+                                                                         bots_in_range} = acc ->
+            new_bots = count_bots_in_range({x, y, z}, bots, factor)
+
+            if new_bots > bots_in_range ||
+                 (new_bots == bots_in_range && distance_from_zero({x, y, z}) < distance) do
+              {{x, y, z}, distance_from_zero({x, y, z}), new_bots}
+            else
+              acc
+            end
+          end)
+        end)
+      end)
+
+    do_part2(
+      bots,
+      {x * factor, (x + 1) * factor},
+      {y * factor, (y + 1) * factor},
+      {z * factor, (z + 1) * factor},
+      div(factor, 10)
+    )
+  end
+
+  defp count_bots_in_range({x1, y1, z1}, bots, f) do
+    Enum.count(bots, fn %{x: x2, y: y2, z: z2, r: r} ->
+      within_radius?(%{x: div(x2, f), y: div(y2, f), z: div(z2, f), r: div(r, f)}, %{
+        x: x1,
+        y: y1,
+        z: z1
+      })
+    end)
   end
 
   defp distance_from_zero({x, y, z}), do: abs(x) + abs(y) + abs(z)
@@ -66,4 +97,5 @@ defmodule Y2018.Day23 do
   end
 
   def part1_verify, do: input() |> parse_input() |> part1() |> elem(1)
+  def part2_verify, do: input() |> parse_input() |> part2()
 end
--- test/y2018/day23_test.exs
+++ test/y2018/day23_test.exs
@@ -4,4 +4,5 @@ defmodule Y2018.Day23Test do
   doctest Day23
 
   test "verification, part 1", do: assert(Day23.part1_verify() == 619)
+  test "verification, part 2", do: assert(Day23.part2_verify() == 71_631_000)
 end
