07f105a227e2abb60b361799467bb9240c1615d4
Rebecca Le <traybaby@gmail.com>
2020-03-20 23:54:50 +0800

    2019 day 15 - Replace :digraph with libgraph - simpler interface

--- lib/y2019/day15.ex
+++ lib/y2019/day15.ex
@@ -3,30 +3,27 @@ defmodule Y2019.Day15 do
 
   alias Y2019.Intcode
 
-  @grid_size 22
   # Inputs and what those inputs do to x/y co-ordinates
   @moves %{1 => {0, 1}, 2 => {0, -1}, 3 => {-1, 0}, 4 => {1, 0}}
 
-  def part1(input, show \\ true) do
+  def part1(input) do
     program =
       input
       |> Intcode.from_string()
       |> Intcode.new()
 
-    g = :digraph.new()
-    :digraph.add_vertex(g, {0, 0})
-    {graph, map} = do_part1([{{0, 0}, program}], g, %{{0, 0} => :start}, show)
+    {graph, map} = do_part1([{{0, 0}, program}], Graph.new(), %{{0, 0} => :start})
     {win, _} = Enum.find(map, fn {_, key} -> key == :win end)
 
     # length is total number of positions in the path, movements is gaps between them
-    length(:digraph.get_short_path(graph, {0, 0}, win)) - 1
+    length(Graph.dijkstra(graph, {0, 0}, win)) - 1
   end
 
-  def do_part1([], g, seen, _), do: {g, seen}
+  def do_part1([], graph, state), do: {graph, state}
 
-  def do_part1([{position, program} | rest], g, seen, show) do
-    {seen, rest} =
-      Enum.reduce(@moves, {seen, rest}, fn {input, change}, {seen, rest} ->
+  def do_part1([{position, program} | rest], graph, state) do
+    {state, graph, rest} =
+      Enum.reduce(@moves, {state, graph, rest}, fn {input, change}, {state, graph, rest} ->
         {[output], new_program} =
           program
           |> Intcode.add_input(input)
@@ -36,43 +33,45 @@ defmodule Y2019.Day15 do
         case output do
           0 ->
             # Wall.
-            {Map.put(seen, new_position(position, change), :wall), rest}
+            {Map.put(state, new_position(position, change), :wall), graph, rest}
 
           1 ->
             # Space.
             new_position = new_position(position, change)
 
-            if Map.has_key?(seen, new_position) do
-              {seen, rest}
+            if Map.has_key?(state, new_position) do
+              {state, graph, rest}
             else
-              :digraph.add_vertex(g, new_position)
-              :digraph.add_edge(g, position, new_position)
-              :digraph.add_edge(g, new_position, position)
-              {Map.put(seen, new_position, :space), [{new_position, new_program} | rest]}
+              {
+                Map.put(state, new_position, :space),
+                Graph.add_edge(graph, position, new_position),
+                [{new_position, new_program} | rest]
+              }
             end
 
           2 ->
             # Found it!!
             new_position = new_position(position, change)
-            :digraph.add_vertex(g, new_position)
-            :digraph.add_edge(g, position, new_position)
-            :digraph.add_edge(g, new_position, position)
-            {Map.put(seen, new_position, :win), []}
+
+            {
+              Map.put(state, new_position, :win),
+              Graph.add_edge(graph, position, new_position),
+              []
+            }
         end
       end)
 
-    if show, do: visualize(seen)
-
-    do_part1(rest, g, seen, show)
+    do_part1(rest, graph, state)
   end
 
   defp new_position({x1, y1}, {x2, y2}), do: {x1 + x2, y1 + y2}
 
-  defp visualize(state, path \\ []) do
+  def visualize(state, path \\ []) do
+    grid_size = 22
     IEx.Helpers.clear()
 
-    for y <- @grid_size..-@grid_size do
-      for x <- -@grid_size..@grid_size do
+    for y <- grid_size..-grid_size do
+      for x <- -grid_size..grid_size do
         to_tile(Map.get(state, {x, y}), Enum.member?(path, {x, y}))
       end
       |> Enum.join()
@@ -87,5 +86,5 @@ defmodule Y2019.Day15 do
   defp to_tile(:start, _), do: "X"
   defp to_tile(:win, _), do: "!"
 
-  def part1_verify, do: input() |> part1(false)
+  def part1_verify, do: input() |> part1()
 end
