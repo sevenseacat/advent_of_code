2f398af2cd9f787d2d347a2ada43ca094583618d
Rebecca Le <traybaby@gmail.com>
2020-12-07 20:07:37 +0800

    2020 day 7, part 2
    The graph idea didn't hold up...

--- lib/y2020/day07.ex
+++ lib/y2020/day07.ex
@@ -23,17 +23,57 @@ defmodule Y2020.Day07 do
   end
 
   defp add_to_graph({outer, inner}, graph) do
-    Enum.reduce(inner, graph, fn {name, count}, acc ->
-      Graph.add_edge(acc, Graph.Edge.new(outer, name, weight: count))
+    Enum.reduce(inner, graph, fn {name, _count}, acc ->
+      Graph.add_edge(acc, Graph.Edge.new(outer, name))
     end)
   end
 
   @doc """
-  iex> Day07.part2(:parsed_input)
-  :ok
+  iex> Day07.part2(%{
+  ...>  "bright white" => %{"shiny gold" => 1},
+  ...>  "dark olive" => %{"dotted black" => 4, "faded blue" => 3},
+  ...>  "dark orange" => %{"bright white" => 3, "muted yellow" => 4},
+  ...>  "dotted black" => %{},
+  ...>  "faded blue" => %{},
+  ...>  "light red" => %{"bright white" => 1, "muted yellow" => 2},
+  ...>  "muted yellow" => %{"faded blue" => 9, "shiny gold" => 2},
+  ...>  "shiny gold" => %{"dark olive" => 1, "vibrant plum" => 2},
+  ...>  "vibrant plum" => %{"dotted black" => 6, "faded blue" => 5}
+  ...> })
+  32
+
+  iex> Day07.part2(%{
+  ...>  "dark blue" => %{"dark violet" => 2},
+  ...>  "dark green" => %{"dark blue" => 2},
+  ...>  "dark orange" => %{"dark yellow" => 2},
+  ...>  "dark red" => %{"dark orange" => 2},
+  ...>  "dark violet" => %{},
+  ...>  "dark yellow" => %{"dark green" => 2},
+  ...>  "shiny gold" => %{"dark red" => 2}
+  ...> })
+  126
   """
-  def part2(_input) do
-    :ok
+  def part2(input, source \\ "shiny gold") do
+    bag_contains(source, input, 1) - 1
+  end
+
+  def bag_contains(bag_name, list, multiplier) when is_binary(bag_name) do
+    bag_contains(
+      {bag_name, Map.get(list, bag_name)},
+      list,
+      multiplier
+    )
+  end
+
+  def bag_contains({_bag, contains}, _list, multiplier) when map_size(contains) == 0,
+    do: multiplier
+
+  def bag_contains({_bag, contains}, list, multiplier) do
+    multiplier +
+      multiplier *
+        Enum.reduce(contains, 0, fn {bag, count}, acc ->
+          acc + bag_contains(bag, list, count)
+        end)
   end
 
   @doc """
@@ -70,4 +110,5 @@ defmodule Y2020.Day07 do
   end
 
   def part1_verify, do: input() |> parse_input() |> part1()
+  def part2_verify, do: input() |> parse_input() |> part2()
 end
