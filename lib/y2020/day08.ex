7468edca7c1ac7ce810efd74d7fc3133d8439343
Rebecca Le <traybaby@gmail.com>
2020-12-08 23:55:06 +0800

    2020 day 8, part 2

--- lib/y2020/day08.ex
+++ lib/y2020/day08.ex
@@ -2,9 +2,9 @@ defmodule Y2020.Day08 do
   use Advent.Day, no: 8
 
   @doc """
-  iex> Day08.part1(%{0 => :noop, 1 => {:acc, 1}, 2 => {:jmp, 4}, 3 => {:acc, 3}, 4 => {:jmp, -3},
+  iex> Day08.part1(%{0 => {:noop, 0}, 1 => {:acc, 1}, 2 => {:jmp, 4}, 3 => {:acc, 3}, 4 => {:jmp, -3},
   ...>              5 => {:acc, -99}, 6 => {:acc, 1}, 7 => {:jmp, -4}, 8 => {:acc, 6}})
-  5
+  {:repeat, 5}
   """
   def part1(input) do
     run_app(input, {0, 0, MapSet.new()})
@@ -12,7 +12,7 @@ defmodule Y2020.Day08 do
 
   defp run_app(input, {pos, val, seen} = state) do
     if MapSet.member?(seen, pos) do
-      val
+      {:repeat, val}
     else
       process_instruction(input, state)
     end
@@ -21,35 +21,46 @@ defmodule Y2020.Day08 do
   defp process_instruction(input, {pos, val, seen} = state) do
     seen = MapSet.put(seen, pos)
 
-    state =
-      case Map.get(input, pos) do
-        nil ->
-          state
+    case Map.get(input, pos) do
+      nil ->
+        {:exit, state}
 
-        :noop ->
-          {pos + 1, val, seen}
+      {:noop, _val} ->
+        run_app(input, {pos + 1, val, seen})
 
-        {:jmp, jmp} ->
-          {pos + jmp, val, seen}
+      {:jmp, jmp} ->
+        run_app(input, {pos + jmp, val, seen})
 
-        {:acc, acc} ->
-          {pos + 1, val + acc, seen}
-      end
-
-    run_app(input, state)
+      {:acc, acc} ->
+        run_app(input, {pos + 1, val + acc, seen})
+    end
   end
 
   @doc """
-  iex> Day08.part2(:parsed_input)
-  :ok
+  iex> Day08.part2(%{0 => {:noop, 0}, 1 => {:acc, 1}, 2 => {:jmp, 4}, 3 => {:acc, 3}, 4 => {:jmp, -3},
+  ...>              5 => {:acc, -99}, 6 => {:acc, 1}, 7 => {:jmp, -4}, 8 => {:acc, 6}})
+  {:exit, 8}
   """
-  def part2(_input) do
-    :ok
+  def part2(input) do
+    input
+    |> Map.keys()
+    |> Enum.reduce([input], fn pos, acc ->
+      case Map.get(input, pos) do
+        {:acc, _} -> acc
+        {:noop, noop} -> [Map.put(input, pos, {:jmp, noop}) | acc]
+        {:jmp, jmp} -> [Map.put(input, pos, {:noop, jmp}) | acc]
+      end
+    end)
+    # One of the inputs will terminate with an {:exit, state} value.
+    |> Enum.find_value(fn input ->
+      {status, state} = run_app(input, {0, 0, MapSet.new()})
+      if status == :exit, do: {status, elem(state, 1)}
+    end)
   end
 
   @doc """
   iex> Day08.parse_input("nop +0\\nacc +1\\njmp -2\\n")
-  %{0 => :noop, 1 => {:acc, 1}, 2 => {:jmp, -2}}
+  %{0 => {:noop, 0}, 1 => {:acc, 1}, 2 => {:jmp, -2}}
   """
   def parse_input(input) do
     input
@@ -59,10 +70,10 @@ defmodule Y2020.Day08 do
     |> Enum.into(%{})
   end
 
-  defp parse_row({"nop" <> _rest, num}), do: {num, :noop}
+  defp parse_row({"nop " <> rest, num}), do: {num, {:noop, String.to_integer(rest)}}
   defp parse_row({"acc " <> rest, num}), do: {num, {:acc, String.to_integer(rest)}}
   defp parse_row({"jmp " <> rest, num}), do: {num, {:jmp, String.to_integer(rest)}}
 
-  def part1_verify, do: input() |> parse_input() |> part1()
-  def part2_verify, do: input() |> parse_input() |> part2()
+  def part1_verify, do: input() |> parse_input() |> part1() |> elem(1)
+  def part2_verify, do: input() |> parse_input() |> part2() |> elem(1)
 end
--- test/y2020/day08_test.exs
+++ test/y2020/day08_test.exs
@@ -4,4 +4,5 @@ defmodule Y2020.Day08Test do
   doctest Day08
 
   test "verification, part 1", do: assert(Day08.part1_verify() == 1684)
+  test "verification, part 2", do: assert(Day08.part2_verify() == 2188)
 end
