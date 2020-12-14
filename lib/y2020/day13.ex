55fd018691f9eb594901f5f70223f2113d667918
Rebecca Le <traybaby@gmail.com>
2020-12-14 14:03:09 +0800

    2020 day 13, part 2 - naive solution that will never finish

--- lib/y2020/day13.ex
+++ lib/y2020/day13.ex
@@ -2,7 +2,7 @@ defmodule Y2020.Day13 do
   use Advent.Day, no: 13
 
   @doc """
-  iex> Day13.part1({939, [7, 13, 59, 31, 19]})
+  iex> Day13.part1({939, [7, 13, nil, nil, 59, nil, 31, 19]})
   %{bus: 59, time: 944, result: 295}
   """
   def part1({time, buses}) do
@@ -18,16 +18,46 @@ defmodule Y2020.Day13 do
   defp lcm_greater_than(min, val, inc), do: lcm_greater_than(min, val + inc, inc)
 
   @doc """
-  iex> Day13.part2(:parsed_input)
-  :ok
+  iex> Day13.part2({939, [7, 13, nil, nil, 59, nil, 31, 19]})
+  1068781
+
+  iex> Day13.part2({0, [17, nil, 13, 19]})
+  3417
+
+  iex> Day13.part2({0, [67, 7, 59, 61]})
+  754018
+
+  iex> Day13.part2({0, [67, nil, 7, 59, 61]})
+  779210
+
+  iex> Day13.part2({0, [67, 7, nil, 59, 61]})
+  1261476
+
+  iex> Day13.part2({0, [1789, 37, 47, 1889]})
+  1202161486
   """
-  def part2(_input) do
-    :ok
+  def part2({_bus_no, buses}) do
+    bus_data =
+      buses
+      |> Enum.with_index()
+      |> Enum.filter(fn {bus, _} -> bus != nil end)
+
+    do_part2(0, bus_data)
+  end
+
+  def do_part2(time, bus_data) do
+    if Enum.all?(bus_data, fn {bus, offset} ->
+         rem(time, bus) == if offset == 0, do: 0, else: bus - offset
+       end) do
+      time
+    else
+      do_part2(time + 1, bus_data)
+    end
   end
 
   @doc """
   iex> Day13.parse_input("939\\n7,13,x,x,59,x,31,19\\n")
-  {939, [7, 13, 59, 31, 19]}
+  {939, [7, 13, nil, nil, 59, nil, 31, 19]}
   """
   def parse_input(input) do
     [time, buses] = String.split(input, "\n", trim: true)
@@ -37,8 +67,7 @@ defmodule Y2020.Day13 do
   defp parse_buses(input) do
     input
     |> String.split(",")
-    |> Enum.filter(fn x -> x != "x" end)
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&if &1 == "x", do: nil, else: String.to_integer(&1))
   end
 
   def part1_verify, do: input() |> parse_input() |> part1() |> Map.get(:result)
