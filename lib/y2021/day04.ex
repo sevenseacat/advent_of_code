a46889177a25c050da8ca9d8a54f064376aa422e
Rebecca Le <traybaby@gmail.com>
2021-12-04 18:07:32 +0800

    2021 day 4, part 1

---
+++ lib/y2021/day04.ex
@@ -0,0 +1,97 @@
+defmodule Y2021.Day04 do
+  use Advent.Day, no: 4
+
+  @called_numbers "13,47,64,52,60,69,80,85,57,1,2,6,30,81,86,40,27,26,97,77,70,92,43,94,8,78,3,88,93,17,55,49,32,59,51,28,33,41,83,67,11,91,53,36,96,7,34,79,98,72,39,56,31,75,82,62,99,66,29,58,9,50,54,12,45,68,4,46,38,21,24,18,44,48,16,61,19,0,90,35,65,37,73,20,22,89,42,23,15,87,74,10,71,25,14,76,84,5,63,95"
+
+  def part1(boards, called \\ @called_numbers) do
+    called
+    |> parse_called()
+    |> play_bingo(boards)
+    |> calculate_score()
+  end
+
+  defp play_bingo([], _boards), do: raise("No winners!")
+
+  defp play_bingo([number | numbers], boards) do
+    boards = Enum.map(boards, &mark_number_on_board(&1, number))
+
+    case Enum.find(boards, &winning_board?/1) do
+      nil -> play_bingo(numbers, boards)
+      board -> {board, number}
+    end
+  end
+
+  defp mark_number_on_board(board, number) do
+    Enum.map(board, &mark_number_on_row(&1, number))
+  end
+
+  defp mark_number_on_row([], _to_mark), do: []
+
+  defp mark_number_on_row([{to_mark, false} | rest], to_mark) do
+    [{to_mark, true} | rest]
+  end
+
+  defp mark_number_on_row([square | rest], to_mark) do
+    [square | mark_number_on_row(rest, to_mark)]
+  end
+
+  defp calculate_score({board, last_num}) do
+    board
+    |> List.flatten()
+    |> Enum.filter(fn {_num, marked} -> !marked end)
+    |> Enum.map(fn {num, _marked} -> num end)
+    |> Enum.sum()
+    |> then(fn num -> num * last_num end)
+  end
+
+  defp winning_board?(board) do
+    winning_row?(board) || winning_column?(board)
+  end
+
+  defp winning_row?(board), do: Enum.any?(board, &all_marked?/1)
+
+  defp winning_column?(board) do
+    board
+    |> transpose()
+    |> winning_row?()
+  end
+
+  defp all_marked?(numbers), do: Enum.all?(numbers, fn {_num, marked} -> marked end)
+
+  # https://stackoverflow.com/a/42887944/560215
+  defp transpose(rows) do
+    rows
+    |> List.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def parse_input(string) do
+    string
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&parse_board/1)
+  end
+
+  defp parse_board(string) do
+    string
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_row/1)
+  end
+
+  defp parse_row(string) do
+    string
+    |> String.trim()
+    |> String.split(~r/\s+/)
+    |> Enum.map(&parse_number/1)
+  end
+
+  # All squares are unmarked (false) by default.
+  defp parse_number(string), do: {String.to_integer(string), false}
+
+  defp parse_called(string) do
+    string
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def part1_verify, do: input() |> parse_input() |> part1()
+end
---
+++ test/y2021/day04_test.exs
@@ -0,0 +1,53 @@
+defmodule Y2021.Day04Test do
+  use ExUnit.Case, async: true
+  alias Y2021.Day04
+  doctest Day04
+
+  test "verification, part 1", do: assert(Day04.part1_verify() == 49686)
+
+  describe "part1/2" do
+    test "works for a full board" do
+      board = [
+        [{14, false}, {21, false}, {17, false}, {24, false}, {4, false}],
+        [{10, false}, {16, false}, {15, false}, {9, false}, {19, false}],
+        [{18, false}, {8, false}, {23, false}, {26, false}, {20, false}],
+        [{22, false}, {11, false}, {13, false}, {6, false}, {5, false}],
+        [{2, false}, {0, false}, {12, false}, {3, false}, {7, false}]
+      ]
+
+      numbers = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1"
+      assert Day04.part1([board], numbers) == 4512
+    end
+  end
+
+  describe "parse_input/1" do
+    test "works for multiple boards" do
+      input = """
+      22 13 17 11  0
+      8  2 23  4 24
+      21  9 14 16  7
+      6 10  3 18  5
+      1 12 20 15 19
+
+      1  2
+      3  4
+      """
+
+      output = [
+        [
+          [{22, false}, {13, false}, {17, false}, {11, false}, {0, false}],
+          [{8, false}, {2, false}, {23, false}, {4, false}, {24, false}],
+          [{21, false}, {9, false}, {14, false}, {16, false}, {7, false}],
+          [{6, false}, {10, false}, {3, false}, {18, false}, {5, false}],
+          [{1, false}, {12, false}, {20, false}, {15, false}, {19, false}]
+        ],
+        [
+          [{1, false}, {2, false}],
+          [{3, false}, {4, false}]
+        ]
+      ]
+
+      assert Day04.parse_input(input) == output
+    end
+  end
+end
