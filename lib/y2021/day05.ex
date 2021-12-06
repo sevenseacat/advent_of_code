7316eeec941a00f4ba9e358e70a8cd8ecff917a8
Rebecca Le <traybaby@gmail.com>
2021-12-06 20:21:47 +0800

    2021 day 5 - a much neater way to calculate points on a diagonal line!

--- lib/y2021/day05.ex
+++ lib/y2021/day05.ex
@@ -23,17 +23,10 @@ defmodule Y2021.Day05 do
     |> to_grid(include_diagonals)
   end
 
-  def to_grid([x, y1, x, y2], _include_diagonals), do: for(y <- y1..y2, do: {x, y})
-  def to_grid([x1, y, x2, y], _include_diagonals), do: for(x <- x1..x2, do: {x, y})
-  def to_grid(_, false), do: nil
-
-  def to_grid([x1, y1, x2, y2], true) do
-    x_mult = to_mult(x1, x2)
-    y_mult = to_mult(y1, y2)
-    for i <- 0..abs(x2 - x1), do: {x1 + i * x_mult, y1 + i * y_mult}
-  end
-
-  def to_mult(val1, val2), do: if(val1 < val2, do: 1, else: -1)
+  defp to_grid([x, y1, x, y2], _include_diagonals), do: for(y <- y1..y2, do: {x, y})
+  defp to_grid([x1, y, x2, y], _include_diagonals), do: for(x <- x1..x2, do: {x, y})
+  defp to_grid(_, false), do: nil
+  defp to_grid([x1, y1, x2, y2], true), do: Enum.zip(x1..x2, y1..y2)
 
   def part1_verify, do: input() |> parse_input(false) |> parts()
   def part2_verify, do: input() |> parse_input(true) |> parts()
