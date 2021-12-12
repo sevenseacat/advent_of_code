af8e8712ed036d303f5c76df15da6fbe7869d6c1
Rebecca Le <traybaby@gmail.com>
2021-12-12 17:16:57 +0800

    Mark verification tests from 2016 day 14 as exceeding test timeout
    They only take like 20 seconds on my laptop, but CI is nowhere near as
    powerful as my laptop...

--- test/y2015/day14_test.exs
+++ test/y2015/day14_test.exs
@@ -3,6 +3,9 @@ defmodule Y2015.Day14Test do
   alias Y2015.Day14
   doctest Day14
 
+  @tag timeout: :infinity
   test "verification, part 1", do: assert(Day14.part1_verify() == 2655)
+
+  @tag timeout: :infinity
   test "verification, part 2", do: assert(Day14.part2_verify() == 1059)
 end
