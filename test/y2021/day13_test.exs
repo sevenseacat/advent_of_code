c86559b1fcad26afdcad5746ea8b9a19eef751df
Rebecca Le <traybaby@gmail.com>
2021-12-13 17:36:16 +0800

    2021 day 13 part 2 - actually verify the ASCII-formatted output

--- test/y2021/day13_test.exs
+++ test/y2021/day13_test.exs
@@ -3,6 +3,21 @@ defmodule Y2021.Day13Test do
   alias Y2021.Day13
   doctest Day13
 
+  import ExUnit.CaptureIO
+
   test "verification, part 1", do: assert(Day13.part1_verify() == 751)
-  test "verification, part 2", do: assert(Day13.part2_verify() == :ok)
+
+  test "verification, part 2" do
+    expected = """
+    🁢🁢🁢...🁢🁢..🁢..🁢.🁢🁢🁢..🁢..🁢.🁢....🁢..🁢.🁢...
+    🁢..🁢.🁢..🁢.🁢..🁢.🁢..🁢.🁢.🁢..🁢....🁢.🁢..🁢...
+    🁢..🁢.🁢....🁢🁢🁢🁢.🁢..🁢.🁢🁢...🁢....🁢🁢...🁢...
+    🁢🁢🁢..🁢.🁢🁢.🁢..🁢.🁢🁢🁢..🁢.🁢..🁢....🁢.🁢..🁢...
+    🁢....🁢..🁢.🁢..🁢.🁢.🁢..🁢.🁢..🁢....🁢.🁢..🁢...
+    🁢.....🁢🁢🁢.🁢..🁢.🁢..🁢.🁢..🁢.🁢🁢🁢🁢.🁢..🁢.🁢🁢🁢🁢
+    """
+
+    actual = capture_io(fn -> Day13.part2_verify() end)
+    assert actual == expected
+  end
 end
