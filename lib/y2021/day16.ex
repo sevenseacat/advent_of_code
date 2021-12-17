cfa6fc40a29027f66f1285dd1851e46423f4b90d
Rebecca Le <traybaby@gmail.com>
2021-12-18 01:36:54 +0800

    Squashed commit of 2021 day 16 refactoring:
    * A better way of parsing a literal, using bitwise shifting :D
    * Use `Enum.product/1` to calculate a value :D
    * You *can* use values matched in pattern matching binaries like this!
      I thought I tried this but it didn't work... José's livestream proves that it does
    * The top-level is only one packet, not a list
      This makes parsing a bit simpler and we can just ignore the zeros at the end

--- lib/y2021/day16.ex
+++ lib/y2021/day16.ex
@@ -1,6 +1,8 @@
 defmodule Y2021.Day16 do
   use Advent.Day, no: 16
 
+  import Bitwise
+
   @doc """
   iex> Day16.part1("D2FE28")
   6
@@ -20,7 +22,6 @@ defmodule Y2021.Day16 do
   def part1(string) do
     string
     |> parse_input
-    |> hd
     |> get_version()
   end
 
@@ -52,7 +53,6 @@ defmodule Y2021.Day16 do
   def part2(string) do
     string
     |> parse_input
-    |> hd()
     |> get_value
   end
 
@@ -69,7 +69,7 @@ defmodule Y2021.Day16 do
 
     case type_id do
       0 -> Enum.sum(values)
-      1 -> Enum.reduce(values, &*/2)
+      1 -> Enum.product(values)
       2 -> Enum.min(values)
       3 -> Enum.max(values)
       5 -> greater_than?(values)
@@ -84,67 +84,59 @@ defmodule Y2021.Day16 do
 
   @doc """
   iex> Day16.parse_input("D2FE28")
-  [%{version: 6, type_id: 4, value: 2021}]
+  %{version: 6, type_id: 4, value: 2021}
 
   iex> Day16.parse_input("38006F45291200")
-  [%{version: 1, type_id: 6, subpackets: [%{type_id: 4, value: 10, version: 6}, %{type_id: 4, value: 20, version: 2}]}]
+  %{version: 1, type_id: 6, subpackets: [%{type_id: 4, value: 10, version: 6}, %{type_id: 4, value: 20, version: 2}]}
 
   iex> Day16.parse_input("EE00D40C823060")
-  [%{version: 7, type_id: 3, subpackets: [
+  %{version: 7, type_id: 3, subpackets: [
     %{type_id: 4, value: 1, version: 2}, %{type_id: 4, value: 2, version: 4}, %{type_id: 4, value: 3, version: 1}
-  ]}]
+  ]}
   """
   def parse_input(string) do
     string
     |> String.trim()
     |> Base.decode16!()
-    |> parse_packets()
+    |> parse_packet()
     |> elem(0)
   end
 
   defp parse_packet(<<version::3, 4::3, rest::bits>>) do
-    {literal, rest} = parse_literal(rest)
-    number = literal_list_to_number(literal)
-    {%{version: version, type_id: 4, value: number}, rest}
+    {literal, rest} = parse_literal(rest, 0)
+    {%{version: version, type_id: 4, value: literal}, rest}
   end
 
   defp parse_packet(<<version::3, type::3, rest::bits>>) do
     {subpackets, rest} = parse_subpackets(rest)
-
     {%{version: version, type_id: type, subpackets: subpackets}, rest}
   end
 
+  # The extra zeros at the end. Ignore.
   defp parse_packet(_) do
-    {[], ""}
+    {[], nil}
   end
-
-  # All the trailing zeros that we can ignore/discard
-  defp parse_packets(""), do: {[], ""}
-  defp parse_packets(<<0::1>>), do: {[], ""}
-  defp parse_packets(<<0::2>>), do: {[], ""}
-  defp parse_packets(<<0::3>>), do: {[], ""}
-  defp parse_packets(<<0::4>>), do: {[], ""}
-  defp parse_packets(<<0::5>>), do: {[], ""}
-  defp parse_packets(<<0::6>>), do: {[], ""}
-  defp parse_packets(<<0::7>>), do: {[], ""}
 
   defp parse_packets(bits) do
     {packet, rest} = parse_packet(bits)
-    {packets, rest} = parse_packets(rest)
-    {[packet | packets], rest}
+
+    case packet do
+      [] ->
+        {[], rest}
+
+      packet ->
+        {packets, rest} = parse_packets(rest)
+        {[packet | packets], rest}
+    end
   end
 
-  defp parse_subpackets(<<0::1, rest::bits>>) do
-    <<subpacket_length::15, rest::bits>> = rest
-    <<subpackets::bits-size(subpacket_length), rest::bits>> = rest
+  defp parse_subpackets(<<0::1, length::15, subpackets::bits-size(length), rest::bits>>) do
     {parse_packets(subpackets) |> elem(0), rest}
   end
 
-  defp parse_subpackets(<<1::1, rest::bits>>) do
-    <<subpacket_count::11, rest::bits>> = rest
-
+  defp parse_subpackets(<<1::1, count::11, rest::bits>>) do
     {packets, rest} =
-      1..subpacket_count
+      1..count
       |> Enum.reduce({[], rest}, fn _, {packets, rest} ->
         {packet, rest} = parse_packet(rest)
         {[packet | packets], rest}
@@ -153,24 +145,13 @@ defmodule Y2021.Day16 do
     {Enum.reverse(packets), rest}
   end
 
-  # Ignore trailing zeros
-  defp parse_literal(<<0::1, rest::bits>>) do
-    <<value::4, rest::bits>> = rest
-    {[value], rest}
+  defp parse_literal(<<0::1, value::4, rest::bits>>, acc) do
+    {(acc <<< 4) + value, rest}
   end
 
-  defp parse_literal(<<1::1, value::4, rest::bits>>) do
-    {next, rest} = parse_literal(rest)
-    {[value | next], rest}
+  defp parse_literal(<<1::1, value::4, rest::bits>>, acc) do
+    parse_literal(rest, (acc <<< 4) + value)
   end
-
-  # Turn a list of numbers read from 4-bit chunks eg. [7, 14, 5] into their actual value -> 2021.
-  defp literal_list_to_number(list), do: make_number(Enum.reverse(list), 0)
-
-  defp make_number([], _), do: 0
-
-  defp make_number([num | nums], power),
-    do: num * Integer.pow(16, power) + make_number(nums, power + 1)
 
   def part1_verify, do: input() |> part1()
   def part2_verify, do: input() |> part2()
