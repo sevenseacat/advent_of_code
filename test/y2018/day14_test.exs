f86300440834a54b4cbb71f1beea73d311d5f0d9
Rebecca Le <traybaby@gmail.com>
2022-09-12 21:01:17 +0800

    Tag slow verification test for 2018 day 14 part 2, so it can run reliably on CI

--- test/y2018/day14_test.exs
+++ test/y2018/day14_test.exs
@@ -4,5 +4,7 @@ defmodule Y2018.Day14Test do
   doctest Day14
 
   test "verification, part 1", do: assert(Day14.part1_verify() == "1132413111")
+
+  @tag timeout: :infinity
   test "verification, part 2", do: assert(Day14.part2_verify() == 20_340_232)
 end
