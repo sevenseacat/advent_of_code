f80731065a51f89f8cdb2f3d653b5ab3e288985e
Rebecca Le <traybaby@gmail.com>
2022-12-18 20:23:14 +0800

    2020 day 17, part 2

--- lib/y2020/day17.ex
+++ lib/y2020/day17.ex
@@ -2,32 +2,31 @@ defmodule Y2020.Day17 do
   use Advent.Day, no: 17
   alias Advent.Grid
 
-  @adjacents for x <- -1..1, y <- -1..1, z <- -1..1, {x, y, z} != {0, 0, 0}, do: {x, y, z}
+  @adjacents_3d for x <- -1..1, y <- -1..1, z <- -1..1, [x, y, z] != [0, 0, 0], do: [x, y, z]
+  @adjacents_4d for x <- -1..1,
+                    y <- -1..1,
+                    z <- -1..1,
+                    w <- -1..1,
+                    [x, y, z, w] != [0, 0, 0, 0],
+                    do: [x, y, z, w]
 
-  def part1(input, rounds \\ 6) do
-    {{min_x, _, _}, {max_x, _, _}} = Map.keys(input) |> Enum.min_max_by(&elem(&1, 0))
-    {{_, min_y, _}, {_, max_y, _}} = Map.keys(input) |> Enum.min_max_by(&elem(&1, 1))
+  def parts(input, dimension) do
+    {{min_x, _}, {max_x, _}} = Map.keys(input) |> Enum.min_max_by(&elem(&1, 0))
+    {{_, min_y}, {_, max_y}} = Map.keys(input) |> Enum.min_max_by(&elem(&1, 1))
 
-    do_rounds(input, rounds, {{min_x, max_x}, {min_y, max_y}, {0, 0}})
+    input = grow_to_dimension(input, dimension)
+    do_rounds(input, 6, dimension, {{min_x, max_x}, {min_y, max_y}, {0, 0}})
   end
 
-  # @doc """
-  # iex> Day17.part2("update or delete me")
-  # "update or delete me"
-  # """
-  # def part2(input) do
-  #   input
-  # end
-
-  defp do_rounds(grid, 0, _ranges) do
+  defp do_rounds(grid, 0, _dimension, _ranges) do
     grid |> Map.values() |> Enum.count(fn v -> v == "#" end)
   end
 
-  defp do_rounds(grid, round, ranges) do
+  defp do_rounds(grid, round, dimension, ranges) do
     ranges = expand_ranges(ranges)
 
     ranges
-    |> coords_in_range()
+    |> coords_in_range(dimension)
     |> Enum.reduce(Map.new(), fn coord, acc ->
       current = Map.get(grid, coord, ".")
       active_adjacent = get_adjacents(coord, grid) |> Enum.count(fn v -> v == "#" end)
@@ -42,31 +41,43 @@ defmodule Y2020.Day17 do
 
       Map.put(acc, coord, new_val)
     end)
-    |> do_rounds(round - 1, ranges)
+    |> do_rounds(round - 1, dimension, ranges)
   end
 
-  defp coords_in_range({{x1, x2}, {y1, y2}, {z1, z2}}) do
-    for x <- x1..x2, y <- y1..y2, z <- z1..z2, do: {x, y, z}
+  defp coords_in_range({{x1, x2}, {y1, y2}, {z1, z2}}, 3) do
+    for x <- x1..x2, y <- y1..y2, z <- z1..z2, do: [x, y, z]
+  end
+
+  defp coords_in_range({{x1, x2}, {y1, y2}, {z1, z2}}, 4) do
+    for x <- x1..x2, y <- y1..y2, z <- z1..z2, w <- z1..z2, do: [x, y, z, w]
   end
 
   defp expand_ranges({xs, ys, zs}), do: {expand_range(xs), expand_range(ys), expand_range(zs)}
   defp expand_range({from, to}), do: {from - 1, to + 1}
 
-  defp get_adjacents({x, y, z}, grid) do
-    Enum.map(@adjacents, fn {a, b, c} ->
-      Map.get(grid, {x + a, y + b, z + c}, ".")
+  defp get_adjacents(coord, grid) when length(coord) == 4 do
+    do_get_adjacents(coord, @adjacents_4d, grid)
+  end
+
+  defp get_adjacents(coord, grid) when length(coord) == 3 do
+    do_get_adjacents(coord, @adjacents_3d, grid)
+  end
+
+  defp do_get_adjacents(coord, adjacents, grid) do
+    Enum.map(adjacents, fn adjacent ->
+      adjacent_coord = Enum.zip([coord, adjacent]) |> Enum.map(fn {a, b} -> a + b end)
+      Map.get(grid, adjacent_coord, ".")
     end)
   end
 
-  def parse_input(input) do
-    # This makes a 2D grid
-    Grid.new(input)
-    # But ours is 3D, so pretend it's at z=0
-    |> Enum.reduce(Map.new(), fn {{row, col}, val}, acc ->
-      Map.put(acc, {row, col, 0}, val)
+  def parse_input(input), do: Grid.new(input)
+
+  defp grow_to_dimension(input, dimension) do
+    Enum.reduce(input, Map.new(), fn {{row, col}, val}, acc ->
+      Map.put(acc, [row, col | List.duplicate(0, dimension - 2)], val)
     end)
   end
 
-  def part1_verify, do: input() |> parse_input() |> part1()
-  # def part2_verify, do: input() |> parse_input() |> part2()
+  def part1_verify, do: input() |> parse_input() |> parts(3)
+  def part2_verify, do: input() |> parse_input() |> parts(4)
 end
--- test/y2020/day17_test.exs
+++ test/y2020/day17_test.exs
@@ -4,11 +4,12 @@ defmodule Y2020.Day17Test do
   doctest Day17
 
   test "verification, part 1", do: assert(Day17.part1_verify() == 230)
-  # test "verification, part 2", do: assert(Day17.part2_verify() == "update or delete me")
+  test "verification, part 2", do: assert(Day17.part2_verify() == 1600)
 
   @sample_input ".#.\n..#\n###"
 
-  test "part1/1" do
-    assert 112 == Day17.parse_input(@sample_input) |> Day17.part1()
+  test "parts/2" do
+    assert 112 == Day17.parse_input(@sample_input) |> Day17.parts(3)
+    assert 848 == Day17.parse_input(@sample_input) |> Day17.parts(4)
   end
 end
