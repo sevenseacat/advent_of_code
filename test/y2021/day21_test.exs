77932e176b00b63752bca15104cb3d2431e7c3d9
Rebecca Le <traybaby@gmail.com>
2022-12-18 15:04:13 +0800

    2021 day 21 test fix
    The doctests intermittently time out on GitHub CI, so just set a timeout
    for the whole module

--- test/y2021/day21_test.exs
+++ test/y2021/day21_test.exs
@@ -1,10 +1,13 @@
 defmodule Y2021.Day21Test do
   use ExUnit.Case, async: true
+
+  # Needs to apply to the doctest for part 2 as well
+  @moduletag timeout: :infinity
+
   alias Y2021.Day21
   doctest Day21
 
   test "verification, part 1", do: assert(Day21.part1_verify() == 893_700)
 
-  @tag timeout: :infinity
   test "verification, part 2", do: assert(Day21.part2_verify() == 568_867_175_661_958)
 end
