a7c91768c131d6c23e341a5e3fa919789c0cd26e
Rebecca Le <traybaby@gmail.com>
2023-12-01 14:11:08 +0800

    2023 day 1 part 2 - that was trickier than it had a right to be

---
+++ lib/y2023/README.md
@@ -0,0 +1,19 @@
+# Advent of Code 2023
+
+My Elixir solutions for [Advent of Code 2023](https://adventofcode.com/2023).
+
+<!-- stars 2023 start --><img src="https://img.shields.io/static/v1?label=2023&message=2%20stars&style=for-the-badge&color=red" alt="2 stars" /><!-- stars 2023 end -->
+
+## Benchmarks
+
+If you're curious how long each of my solutions roughly takes to run.
+
+You can check out the code yourself and run the benchmarks with `mix bench 2023`!
+
+(Benchmarking results sorted manually for ease of read)
+
+```
+Name                     ips        average  deviation         median         99th %
+day 01, part 1        110.07        9.09 ms     ±1.38%        9.07 ms        9.69 ms
+day 01, part 2         85.23       11.73 ms     ±0.77%       11.71 ms       11.96 ms
+```
--- lib/y2023/day01.ex
+++ lib/y2023/day01.ex
@@ -1,6 +1,18 @@
 defmodule Y2023.Day01 do
   use Advent.Day, no: 01
 
+  @word_to_number %{
+    "one" => "1",
+    "two" => "2",
+    "three" => "3",
+    "four" => "4",
+    "five" => "5",
+    "six" => "6",
+    "seven" => "7",
+    "eight" => "8",
+    "nine" => "9"
+  }
+
   @doc """
   iex> Day01.part1(["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"])
   142
@@ -11,18 +23,44 @@ defmodule Y2023.Day01 do
     |> Enum.sum()
   end
 
-  # @doc """
-  # iex> Day01.part2("update or delete me")
-  # "update or delete me"
-  # """
-  # def part2(input) do
-  #   input
-  # end
+  @doc """
+  iex> Day01.part2(["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four",
+  ...> "4nineeightseven2", "zoneight234", "7pqrstsixteen"])
+  281
+
+  iex> Day01.part2(["fourtwone"])
+  41
+  """
+  def part2(input) do
+    input
+    |> Enum.map(&to_part2_integer/1)
+    |> Enum.sum()
+  end
 
   defp to_integer(string) do
     numbers = Regex.replace(~r/[a-z]/, string, "")
     String.to_integer("#{String.first(numbers)}#{String.last(numbers)}")
   end
+
+  defp to_part2_integer(string) do
+    numbers =
+      find_all_numbers(string, Enum.map(0..9, &to_string/1) ++ Map.keys(@word_to_number))
+      |> Enum.filter(& &1)
+
+    String.to_integer("#{List.first(numbers)}#{List.last(numbers)}")
+  end
+
+  defp find_all_numbers("", _matches), do: []
+
+  # Some numbers may be overlapping! eg. "twone" has both two and one - two is first, one is last
+  defp find_all_numbers(string, matches) do
+    [
+      as_number(Enum.find(matches, &String.starts_with?(string, &1)))
+      | find_all_numbers(String.slice(string, 1..-1), matches)
+    ]
+  end
+
+  def as_number(val), do: Map.get(@word_to_number, val, val)
 
   def parse_input(input) do
     input
@@ -30,5 +68,5 @@ defmodule Y2023.Day01 do
   end
 
   def part1_verify, do: input() |> parse_input() |> part1()
-  # def part2_verify, do: input() |> parse_input() |> part2()
+  def part2_verify, do: input() |> parse_input() |> part2()
 end
