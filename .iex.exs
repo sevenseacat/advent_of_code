5e7d986e51a7ad526602fa285b56702d6467a834
Rebecca Le <traybaby@gmail.com>
2023-12-09 18:43:58 +0800

    Change iex aliases to be for 2023

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,3 @@
-alias Y2022.{Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10}
-alias Y2022.{Day11, Day12, Day13, Day14, Day15, Day16, Day17, Day18, Day19, Day20}
-alias Y2022.{Day21, Day22, Day23, Day24, Day25}
+alias Y2023.{Day01, Day02, Day03, Day04, Day05, Day06, Day07, Day08, Day09, Day10}
+alias Y2023.{Day11, Day12, Day13, Day14, Day15, Day16, Day17, Day18, Day19, Day20}
+alias Y2023.{Day21, Day22, Day23, Day24, Day25}
