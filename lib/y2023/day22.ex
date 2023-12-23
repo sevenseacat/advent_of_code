096f17d6ed4e25cbbca456d0640c99bbf3de2598
Rebecca Le <traybaby@gmail.com>
2023-12-23 14:38:04 +0800

    2023 day 22, part 2
    Accidentally did most of this during part 1 - just needed to count the
    differences instead of seeing if there are any

--- lib/y2023/day22.ex
+++ lib/y2023/day22.ex
@@ -2,33 +2,26 @@ defmodule Y2023.Day22 do
   use Advent.Day, no: 22
 
   def part1(pieces) do
-    fallen_pieces =
-      pieces
-      |> Enum.map(&add_cubes/1)
-      |> fall()
+    fallen = fall(pieces)
 
-    fallen_pieces
+    fallen
     |> Enum.count(fn {index, _piece} ->
-      pieces = Map.delete(fallen_pieces, index)
+      pieces = Map.delete(fallen, index)
       pieces == fall(pieces)
     end)
   end
 
-  # @doc """
-  # iex> Day22.part2("update or delete me")
-  # "update or delete me"
-  # """
-  # def part2(input) do
-  #   input
-  # end
+  def part2(pieces) do
+    fallen = fall(pieces)
 
-  defp add_cubes({%{from: {x1, y1, z1}, to: {x2, y2, z2}} = piece, index}) do
-    cubes =
-      for x <- x1..x2, y <- y1..y2, z <- z1..z2 do
-        {x, y, z}
-      end
-
-    {index, Map.put(piece, :cubes, MapSet.new(cubes))}
+    fallen
+    |> Enum.map(fn {index, _piece} ->
+      fallen
+      |> Map.delete(index)
+      |> fall()
+      |> Enum.count(fn {index, piece} -> piece != Map.get(fallen, index) end)
+    end)
+    |> Enum.sum()
   end
 
   defp fall(pieces) do
@@ -51,7 +44,6 @@ defmodule Y2023.Day22 do
             # This is a valid place to fall
             {:cont, fallen_piece}
           else
-            # IO.puts("nooooope.")
             # This is not a valid place to fall
             {:halt, piece}
           end
@@ -73,8 +65,8 @@ defmodule Y2023.Day22 do
 
   @doc """
   iex> Day22.parse_input("1,0,1~1,2,1\\n0,0,2~2,0,2")
-  %{%{from: {1, 0, 1}, to: {1, 2, 1}} => 0,
-  %{from: {0, 0, 2}, to: {2, 0, 2}} => 1}
+  %{0 => %{from: {1,0,1}, to: {1,2,1}, cubes: MapSet.new([{1,0,1}, {1,1,1}, {1,2,1}])},
+  1 => %{from: {0,0,2}, to: {2,0,2}, cubes: MapSet.new([{0,0,2}, {1,0,2}, {2,0,2}])}}
   """
   def parse_input(input) do
     for row <- String.split(input, "\n", trim: true) do
@@ -82,14 +74,19 @@ defmodule Y2023.Day22 do
         String.split(row, ["~", ","])
         |> Enum.map(&String.to_integer/1)
 
-      %{from: {x1, y1, z1}, to: {x2, y2, z2}}
+      cubes =
+        for x <- x1..x2, y <- y1..y2, z <- z1..z2 do
+          {x, y, z}
+        end
+
+      %{from: {x1, y1, z1}, to: {x2, y2, z2}, cubes: MapSet.new(cubes)}
     end
-    |> Enum.with_index()
+    |> Enum.with_index(fn element, index -> {index, element} end)
     |> Map.new()
   end
 
   defp z({_, _, z}, offset \\ 0), do: z - offset
 
   def part1_verify, do: input() |> parse_input() |> part1()
-  # def part2_verify, do: input() |> parse_input() |> part2()
+  def part2_verify, do: input() |> parse_input() |> part2()
 end
--- test/y2023/day22_test.exs
+++ test/y2023/day22_test.exs
@@ -4,7 +4,7 @@ defmodule Y2023.Day22Test do
   doctest Day22
 
   test "verification, part 1", do: assert(Day22.part1_verify() == 507)
-  # test "verification, part 2", do: assert(Day22.part2_verify() == "update or delete me")
+  test "verification, part 2", do: assert(Day22.part2_verify() == 51733)
 
   @sample_input """
   1,0,1~1,2,1
@@ -18,5 +18,9 @@ defmodule Y2023.Day22Test do
 
   test "part 1" do
     assert Day22.parse_input(@sample_input) |> Day22.part1() == 5
+  end
+
+  test "part 2" do
+    assert Day22.parse_input(@sample_input) |> Day22.part2() == 7
   end
 end
