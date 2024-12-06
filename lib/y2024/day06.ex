d021a9cbd115a8cc26a41b7e25f0bdfb0eb84068
Rebecca Le <traybaby@gmail.com>
2024-12-06 21:28:08 +0800

    2024 day 6 - more optimizations
    We don't care about aaaaallll the floor spots, we only care about the walls -
    they can be stored in a much more compact MapSet for faster retrieval

--- lib/y2024/day06.ex
+++ lib/y2024/day06.ex
@@ -2,57 +2,63 @@ defmodule Y2024.Day06 do
   use Advent.Day, no: 06
   alias Advent.Grid
 
-  def part1(grid) do
-    {grid, guard} = find_guard(grid)
-
-    move(grid, guard, :up, MapSet.new())
-    |> MapSet.delete(guard)
+  def part1(%{walls: walls, guard: guard, size: size}) do
+    move(walls, guard, :up, size, MapSet.new())
     |> MapSet.size()
+    # Don't include the last out-of-grid step
+    |> Kernel.-(1)
   end
 
-  def part2(grid) do
-    {grid, guard} = find_guard(grid)
-
-    move(grid, guard, :up, MapSet.new())
+  def part2(%{walls: walls, guard: guard, size: size}) do
+    move(walls, guard, :up, size, MapSet.new())
     |> Task.async_stream(fn coord ->
-      coord != guard && floor?(grid, coord) &&
-        check_loop(add_wall(grid, coord), guard, :up, MapSet.new())
+      coord != guard && !wall?(walls, coord) &&
+        check_loop(add_wall(walls, coord), guard, :up, size, MapSet.new())
     end)
     |> Enum.count(fn {:ok, val} -> val end)
   end
 
-  defp move(grid, guard_position, facing, seen) do
-    if !in_grid?(grid, guard_position) do
+  defp move(walls, guard_position, facing, size, seen) do
+    if out_of_grid?(size, guard_position) do
       seen
     else
-      {next_pos, next_facing} = move_forward_or_turn(grid, guard_position, facing)
-      move(grid, next_pos, next_facing, MapSet.put(seen, next_pos))
+      {next_pos, next_facing} = move_forward_or_turn(walls, guard_position, facing)
+      move(walls, next_pos, next_facing, size, MapSet.put(seen, next_pos))
     end
   end
 
-  defp check_loop(grid, guard_position, facing, seen) do
-    if !in_grid?(grid, guard_position) do
+  defp check_loop(walls, guard_position, facing, size, seen) do
+    if out_of_grid?(size, guard_position) do
       false
     else
-      {next_pos, next_facing} = move_forward_or_turn(grid, guard_position, facing)
+      {next_pos, next_facing} = move_forward_or_turn(walls, guard_position, facing)
 
       if MapSet.member?(seen, {next_pos, next_facing}) do
         true
       else
-        check_loop(grid, next_pos, next_facing, MapSet.put(seen, {next_pos, next_facing}))
+        check_loop(walls, next_pos, next_facing, size, MapSet.put(seen, {next_pos, next_facing}))
       end
     end
   end
 
   def parse_input(input) do
-    Grid.new(input)
+    grid = Grid.new(input)
+    guard = find_guard(grid)
+
+    walls =
+      grid
+      |> Enum.filter(fn {_coord, val} -> val == "#" end)
+      |> Enum.map(&elem(&1, 0))
+      |> MapSet.new()
+
+    %{size: Grid.size(grid), walls: walls, guard: guard}
   end
 
-  defp move_forward_or_turn(grid, position, facing) do
+  defp move_forward_or_turn(walls, position, facing) do
     next_position = forward(position, facing)
 
-    if wall?(grid, next_position) do
-      move_forward_or_turn(grid, position, turn_right(facing))
+    if wall?(walls, next_position) do
+      move_forward_or_turn(walls, position, turn_right(facing))
     else
       {next_position, facing}
     end
@@ -69,17 +75,16 @@ defmodule Y2024.Day06 do
   defp turn_right(:left), do: :up
 
   defp find_guard(grid) do
-    guard =
-      Enum.find(grid, fn {_coord, key} -> key == "^" end)
-      |> elem(0)
-
-    {Map.put(grid, guard, "."), guard}
+    Enum.find(grid, fn {_coord, key} -> key == "^" end)
+    |> elem(0)
   end
 
-  defp wall?(grid, position), do: Map.get(grid, position) == "#"
-  defp floor?(grid, position), do: Map.get(grid, position) == "."
-  defp in_grid?(grid, position), do: Map.get(grid, position) != nil
-  defp add_wall(grid, position), do: Map.put(grid, position, "#")
+  defp wall?(walls, position), do: MapSet.member?(walls, position)
+  defp add_wall(walls, position), do: MapSet.put(walls, position)
+
+  defp out_of_grid?({max_row, max_col}, {row, col}) do
+    row < 0 || row > max_row || col < 0 || col > max_col
+  end
 
   def part1_verify, do: input() |> parse_input() |> part1()
   def part2_verify, do: input() |> parse_input() |> part2()
