a6e32f43389c7c532034e95b824e3ed4e77b85b2
Rebecca Le <traybaby@gmail.com>
2024-12-07 14:04:31 +0800

    2024 day 7 - radically simplify the code, its now easier to understand and a lot quicker!

--- lib/y2024/day07.ex
+++ lib/y2024/day07.ex
@@ -9,33 +9,18 @@ defmodule Y2024.Day07 do
     do_parts(input, [&Kernel.+/2, &Kernel.*/2, &concat/2])
   end
 
-  defp do_parts(input, operands) do
+  defp do_parts(input, operators) do
     input
-    |> Task.async_stream(fn {total, list} -> {total, valid?({total, list}, operands)} end)
+    |> Task.async_stream(fn {total, list} -> {total, valid?({total, list}, operators)} end)
     |> Enum.reduce(0, fn {:ok, {value, result}}, acc ->
       if result, do: acc + value, else: acc
     end)
   end
 
-  def valid?({total, list}, operands) do
-    check_valid(total, [list], [], operands)
-  end
+  def valid?({total, [num]}, _), do: total == num
 
-  def check_valid(_total, [], [], _), do: false
-
-  def check_valid(total, [], next, operands), do: check_valid(total, next, [], operands)
-
-  def check_valid(total, [[num] | next], [], operands) do
-    num == total || check_valid(total, next, [], operands)
-  end
-
-  def check_valid(total, [[num1, num2 | rest] | next], next_level, operands) do
-    if num1 > total || num2 > total do
-      check_valid(total, next, next_level, operands)
-    else
-      next_states = Enum.map(operands, fn op -> [op.(num1, num2) | rest] end)
-      check_valid(total, next, next_states ++ next_level, operands)
-    end
+  def valid?({total, [num1, num2 | list]}, operators) do
+    Enum.any?(operators, fn op -> valid?({total, [op.(num1, num2) | list]}, operators) end)
   end
 
   def concat(one, two), do: "#{one}#{two}" |> String.to_integer()
