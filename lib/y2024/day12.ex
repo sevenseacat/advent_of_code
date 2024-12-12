200470199033456a56be289f2867fa17275fe603
Rebecca Le <traybaby@gmail.com>
2024-12-12 18:44:47 +0800

    2024 day 12, part 2

--- lib/y2024/day12.ex
+++ lib/y2024/day12.ex
@@ -6,24 +6,27 @@ defmodule Y2024.Day12 do
     |> find_regions()
     |> find_borders(grid)
     |> Enum.map(fn {plot, regions} ->
-      clean_regions =
-        regions
-        |> Enum.map(fn region ->
-          %{area: MapSet.size(region.coords), border: region.border}
-        end)
-
-      {plot, clean_regions}
+      {plot,
+       Enum.map(regions, fn region ->
+         %{area: MapSet.size(region.coords), border: MapSet.size(region.borders)}
+       end)}
     end)
     |> Map.new()
   end
 
-  # @doc """
-  # iex> Day12.part2("update or delete me")
-  # "update or delete me"
-  # """
-  # def part2(input) do
-  #   input
-  # end
+  def part2(grid) do
+    grid
+    |> find_regions()
+    |> find_borders(grid)
+    |> find_sides()
+    |> Enum.map(fn {plot, regions} ->
+      {plot,
+       Enum.map(regions, fn region ->
+         %{area: MapSet.size(region.coords), sides: length(region.sides)}
+       end)}
+    end)
+    |> Map.new()
+  end
 
   def parse_input(input) do
     Advent.Grid.new(input)
@@ -34,22 +37,47 @@ defmodule Y2024.Day12 do
     |> Enum.group_by(fn {_coord, plot} -> plot end)
     |> Task.async_stream(fn {plot, list} ->
       coords = Enum.map(list, fn {coord, _plot} -> coord end)
-      {plot, group_regions(coords) |> Enum.map(fn coords -> %{coords: coords} end)}
+
+      {plot,
+       group_connecting(coords)
+       |> Enum.map(fn coords -> %{coords: coords} end)}
+    end)
+    |> Enum.reduce(%{}, fn {:ok, {plot, regions}}, acc -> Map.put(acc, plot, regions) end)
+  end
+
+  def find_sides(map) do
+    map
+    |> Task.async_stream(fn {plot, list} ->
+      {plot,
+       Enum.map(list, fn region ->
+         sides =
+           region.borders
+           |> Enum.group_by(fn {_, dir} -> dir end)
+           |> Enum.flat_map(fn {_, list} ->
+             list
+             |> Enum.map(fn {coord, _} -> coord end)
+             |> group_connecting()
+           end)
+
+         Map.put(region, :sides, sides)
+       end)}
     end)
     |> Enum.reduce(%{}, fn {:ok, {plot, regions}}, acc -> Map.put(acc, plot, regions) end)
   end
 
   # There's probably a better way to do this - grabbing a coord and using a bfs to find all connecting
-  # coords to it, one by one. When all connecting coords are found, start a new region search with
+  # coords to it, one by one. When all connecting coords are found, start a new search with
   # what's left
-  defp group_regions(coords), do: do_group_regions([hd(coords)], tl(coords), [MapSet.new()])
+  defp group_connecting(coords),
+    do: do_group_connecting([hd(coords)], tl(coords), [MapSet.new()])
 
-  defp do_group_regions([], [], list), do: list
+  defp do_group_connecting([], [], list), do: list
 
-  defp do_group_regions([], coords, list),
-    do: do_group_regions([hd(coords)], tl(coords), [MapSet.new() | list])
+  defp do_group_connecting([], coords, list) do
+    do_group_connecting([hd(coords)], tl(coords), [MapSet.new() | list])
+  end
 
-  defp do_group_regions([{row, col} | coords], rest, list) do
+  defp do_group_connecting([{row, col} | coords], rest, list) do
     {adjacent, not_adjacent} =
       Enum.split_with(rest, fn rest ->
         Enum.any?([{0, -1}, {0, 1}, {-1, 0}, {1, 0}], fn {offset_row, offset_col} ->
@@ -57,7 +85,7 @@ defmodule Y2024.Day12 do
         end)
       end)
 
-    do_group_regions(adjacent ++ coords, not_adjacent, [
+    do_group_connecting(adjacent ++ coords, not_adjacent, [
       MapSet.put(hd(list), {row, col}) | tl(list)
     ])
   end
@@ -67,32 +95,42 @@ defmodule Y2024.Day12 do
     |> Enum.map(fn {plot, plot_regions} ->
       {plot,
        Enum.map(plot_regions, fn plot_region ->
-         border =
-           Enum.reduce(plot_region.coords, 0, fn coord, acc ->
-             acc + find_new_borders(grid, coord, plot)
+         borders =
+           Enum.reduce(plot_region.coords, MapSet.new(), fn coord, acc ->
+             grid
+             |> find_new_borders(coord, plot)
+             |> Enum.reduce(acc, fn coord, acc -> MapSet.put(acc, coord) end)
            end)
 
-         Map.put(plot_region, :border, border)
+         Map.put(plot_region, :borders, borders)
        end)}
     end)
     |> Map.new()
   end
 
   defp find_new_borders(map, {row, col}, plot) do
-    [{0, -1}, {0, 1}, {-1, 0}, {1, 0}]
-    |> Enum.count(fn {offset_row, offset_col} ->
+    [
+      {{0, -1}, :left},
+      {{0, 1}, :right},
+      {{-1, 0}, :up},
+      {{1, 0}, :down}
+    ]
+    |> Enum.filter(fn {{offset_row, offset_col}, _} ->
       Map.get(map, {row + offset_row, col + offset_col}) != plot
+    end)
+    |> Enum.map(fn {{offset_row, offset_col}, dir} ->
+      {{row + offset_row, col + offset_col}, dir}
     end)
   end
 
-  defp to_number(map) do
+  defp calculate_cost(map, [one, two]) do
     Enum.reduce(map, 0, fn {_plot, map}, acc ->
-      Enum.reduce(map, acc, fn %{area: area, border: border}, acc ->
-        acc + border * area
+      Enum.reduce(map, acc, fn record, acc ->
+        acc + Map.fetch!(record, one) * Map.fetch!(record, two)
       end)
     end)
   end
 
-  def part1_verify, do: input() |> parse_input() |> part1() |> to_number()
-  # def part2_verify, do: input() |> parse_input() |> part2()
+  def part1_verify, do: input() |> parse_input() |> part1() |> calculate_cost([:border, :area])
+  def part2_verify, do: input() |> parse_input() |> part2() |> calculate_cost([:sides, :area])
 end
--- test/y2024/day12_test.exs
+++ test/y2024/day12_test.exs
@@ -31,6 +31,14 @@ defmodule Y2024.Day12Test do
   MMMISSJEEE
   """
 
+  @sample4 """
+  EEEEE
+  EXXXX
+  EEEEE
+  EXXXX
+  EEEEE
+  """
+
   test "part1/1" do
     expected1 = %{
       "A" => [%{area: 4, border: 10}],
@@ -69,6 +77,29 @@ defmodule Y2024.Day12Test do
     assert Day12.parse_input(@sample3) |> Day12.part1() == expected3
   end
 
+  test "part2/2" do
+    expected1 = %{
+      "A" => [%{area: 4, sides: 4}],
+      "B" => [%{area: 4, sides: 4}],
+      "C" => [%{area: 4, sides: 8}],
+      "D" => [%{area: 1, sides: 4}],
+      "E" => [%{area: 3, sides: 4}]
+    }
+
+    expected4 = %{
+      "X" => [
+        %{area: 4, sides: 4},
+        %{area: 4, sides: 4}
+      ],
+      "E" => [
+        %{area: 17, sides: 12}
+      ]
+    }
+
+    assert Day12.parse_input(@sample1) |> Day12.part2() == expected1
+    assert Day12.parse_input(@sample4) |> Day12.part2() == expected4
+  end
+
   test "verification, part 1", do: assert(Day12.part1_verify() == 1_375_476)
-  # test "verification, part 2", do: assert(Day12.part2_verify() == "update or delete me")
+  test "verification, part 2", do: assert(Day12.part2_verify() == 821_372)
 end
