5c91c6e1a3406a09c0034d1ff1b331f860df8914
Rebecca Le <traybaby@gmail.com>
2025-12-12 23:40:11 +0800

    2024 day 23 part 1 - a super rough and dirty implementation that I'll probably rewrite after I see part 2

---
+++ test/y2024/day23_test.exs
@@ -0,0 +1,47 @@
+defmodule Y2024.Day23Test do
+  use ExUnit.Case, async: true
+  alias Y2024.Day23
+  doctest Day23
+
+  test "verification, part 1", do: assert(Day23.part1_verify() == 893)
+  # test "verification, part 2", do: assert(Day23.part2_verify() == "update or delete me")
+
+  @sample """
+  kh-tc
+  qp-kh
+  de-cg
+  ka-co
+  yn-aq
+  qp-ub
+  cg-tb
+  vc-aq
+  tb-ka
+  wh-tc
+  yn-cg
+  kh-ub
+  ta-co
+  de-co
+  tc-td
+  tb-wq
+  wh-td
+  ta-ka
+  td-qp
+  aq-cg
+  wq-ub
+  ub-vc
+  de-ta
+  wq-aq
+  wq-vc
+  wh-yn
+  ka-de
+  kh-ta
+  co-tc
+  wh-qp
+  tb-vc
+  td-yn
+  """
+
+  test "part1" do
+    assert Day23.parse_input(@sample) |> Day23.part1() == 7
+  end
+end
